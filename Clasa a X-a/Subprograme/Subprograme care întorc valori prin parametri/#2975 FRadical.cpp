ce3acded1ff4e551d110cf9e9123b4d2d4850841
David Pop <33874867+david7295@users.noreply.github.com>
2019-09-22 21:24:47 +0300

    Update #2975 FRadical.cpp

--- Clasa a X-a/Subprograme/Subprograme care întorc valori prin parametri/#2975 FRadical.cpp
+++ Clasa a X-a/Subprograme/Subprograme care întorc valori prin parametri/#2975 FRadical.cpp
@@ -1,26 +1,26 @@
-void FRadical(int n, int &x, int &y)
+void FRadical (int n, int &x, int &y)
 {
     x = 1;
     y = 1;
     int divizor = 2;
-    while(n > 1)
-    {
+    while (n > 1) {
+        
         int exponent = 0;
-        while(n % divizor == 0)
-        {
+        while (n % divizor == 0) {
+            
             exponent ++;
             n = n / divizor;
-        }
-        if(exponent)
-        {
-            if(exponent % 2 == 0)
-                x = x * pow(divizor, exponent / 2);
-            else
-            {
+    }
+        if (exponent) {
+            
+            if (exponent % 2 == 0)
+                x = x * pow (divizor, exponent / 2);
+            else {
+                
                 y = y * divizor;
-                exponent--;
-                if(exponent)
-                    x = x * pow(divizor, exponent / 2);
+                exponent --;
+                if (exponent)
+                    x = x * pow (divizor, exponent / 2);
             }
         }
         divizor ++;
