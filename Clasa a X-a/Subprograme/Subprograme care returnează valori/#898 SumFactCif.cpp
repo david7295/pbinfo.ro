951a86d79b25aa639d5f799d78e528cdb08ffd43
David Pop <33874867+skylake59@users.noreply.github.com>
2019-09-22 19:22:16 +0300

    Add files via upload

---
+++ Clasa a X-a/Subprograme/Subprograme care returnează valori/#898 SumFactCif.cpp
@@ -0,0 +1,19 @@
+int fact (int n) {
+    int p = 1;
+    for (int i = 1; i <= n; i++)
+        p *= i;
+    return p;
+}
+
+    int sumfactcif (int n) {
+        int s = 0;
+        if (n == 0) 
+            return 1;
+        else {
+          while (n > 0) {
+            s += fact (n%10);
+            n = n / 10;
+        }
+    return s;
+        }
+    }
