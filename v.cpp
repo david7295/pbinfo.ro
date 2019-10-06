63119d1e6c68fdab8e02b8f2ea57791f70a25238
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 20:55:42 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 
 using namespace std;
-long long n,p,f;
-int main()
 
-{
-   cin >> n >> p;
-   f=n;
-   while (f<p){
-      cout << f << " ";
-      f=f*n;
-   }
-   if (f==p) cout << f;
-
+int main() {
+   short n, i, j;
+   cin >> n;
+   for (i=1; i<n; ++i) {
+       for (j=1; j<=2*n-1; ++j) {
+           if (j<i || j>2*n-i) cout << "#";
+           else cout << "*";
+       }
+       cout << "\n";
+   }
+   for (i=n+1; i<=2*n; ++i) {
+       for (j=1; j<=2*n-1; ++j) {
+           if (j<i && j>2*n-i) cout << "*";
+           else cout << "#";
+       }
+       cout << "\n";
+   }
+   return 0;
 }
