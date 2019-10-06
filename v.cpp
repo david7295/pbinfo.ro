3294db3eb48749d24950e2a20da0eb1f4cd39133
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 14:50:51 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -2,60 +2,28 @@
 
 using namespace std;
 
-int n,m,i,j,lp,lu,minp,minu, v[50][50];
+int n,m,i,j,x,gasit, v[100][100];
 
-int main()
-
-{
-
+int main() {
    cin >> n >> m;
-
-   for (i=0; i<n; ++i)
-
+   for (i=0; i<n; ++i) {
        for (j=0; j<m; ++j)
-
            cin >> v[i][j];
-
-   minp=v[0][0]; lp=0;
-
-   for (i=1; i<n; ++i)
-
-       if (v[i][0]<minp)
-
-   {
-
-       minp=v[i][0]; lp=i;
-
    }
+   cin >> x;
+   for (j=0; j<m; ++j){ 
+     if (v[0][j]==x || v[n-1][j]==x) { 
+       gasit=1; break;
+     }
+   }
 
-   minu=v[0][m-1]; lu=0;
+   if (!gasit) {
+          for (i=1; i<n-1; ++i) {
+            if (v[i][0]==x || v[i][m-1]==x) { gasit=1; break; }
+          }
+      }
+   if (gasit) cout << "DA";
 
-   for (i=1; i<n; ++i)
-
-       if (v[i][m-1]<minu)
-
-   {
-
-       minu=v[i][m-1]; lu=i;
-
-   }
-
-   int t=v[lp][0];
-
-   v[lp][0]=v[lu][m-1];
-
-   v[lu][m-1]=t;
-
-   for (i=0; i<n; ++i)
-
-   {
-
-       for (j=0; j<m; ++j)
-
-           cout << v[i][j] << " ";
-
-       cout << "\n";
-
-   }
+   else cout << "NU";
 
 }
