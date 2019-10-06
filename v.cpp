41f68251e421f227b4597762133bf915a7668366
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 21:06:04 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 
 using namespace std;
-
-long long int n, m, nrcif, z=1, primajum, douajum;
+unsigned long long  n, A, R, scor=0, i, t;
 int main(){
-   cin >> n;
-   m=n;
-   while (m>9){
-       ++nrcif; m/=10;
-       z*=10;
- }
-   int k=nrcif/2;
-   for (m=1; m<=k; ++m) z/=10;
-   douajum=n%z;
-   primajum=n/(z*10);
-   m=primajum*z+douajum;
-   cout<<m;
-  return 0;
+
+   cin >> n;
+   for (i=1; i<=n; ++i) {
+       cin >> A >> R;
+       t=A*A+3*A+1;
+       if (t==R) ++scor;
+   }
+   cout<<scor;
+   return 0;
 }
