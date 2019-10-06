d0ad6198f94bd28d8c434ddc248e0404043974b4
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 14:28:55 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
+int x[100][100],v[1000000];
+int main()
+{
+    int n,m,nrmaxim=0,solutii[100],z=0,maximap=0;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+        {
+            cin>>x[i][j];
+            v[x[i][j]]++;
+            if(x[i][j]>nrmaxim) nrmaxim=x[i][j];
+        }
 
-int m[511][511];
-
-// L dimensiunea laturii
-// scriem in m o matrice centrata in (l, c)
-// n este val care se scrie in centru
-
-void pat(int n, int L, int l, int c) {
-  L /= 2;
-  m[l][c] = n;
-  if (n > 1) {
-    int semiLatura = L / 2 + 1;
-    pat(n - 1, L, l - semiLatura, c - semiLatura);
-    pat(n - 1, L, l - semiLatura, c + semiLatura);
-    pat(n - 1, L, l + semiLatura, c - semiLatura);
-    pat(n - 1, L, l + semiLatura, c + semiLatura);
-  }
-}
-
-int main() {
-  int n;
-  cin >> n;
-  int L = pow(2, n) - 1;
-  pat(n, L, L / 2, L / 2);
-  for(int i = 0; i < L; i++) {
-    for(int j = 0; j < L; j++)
-      cout << m[i][j] << ' ';
-    cout << '\n';
-  }
-  return 0;
+    for(int i=1;i<=nrmaxim;i++)
+        if(v[i]>maximap) maximap=v[i];
+    for(int i=1;i<=nrmaxim;i++)
+    {
+        if(v[i]==maximap)
+        {
+            z++;
+            solutii[z]=i;
+        }
+    }
+    for(int i=1;i<=z;i++)
+        cout<<solutii[i]<<" ";
+    return 0;
 }
