9a7b494ed61a4ebb807cac20c236cf999c35f030
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 14:30:26 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 
 using namespace std;
-int x[100][100],v[1000000];
-int main()
-{
-    int n,m,nrmaxim=0,solutii[100],z=0,maximap=0;
-    cin>>n>>m;
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-        {
-            cin>>x[i][j];
-            v[x[i][j]]++;
-            if(x[i][j]>nrmaxim) nrmaxim=x[i][j];
-        }
 
-    for(int i=1;i<=nrmaxim;i++)
-        if(v[i]>maximap) maximap=v[i];
-    for(int i=1;i<=nrmaxim;i++)
-    {
-        if(v[i]==maximap)
-        {
-            z++;
-            solutii[z]=i;
-        }
-    }
-    for(int i=1;i<=z;i++)
-        cout<<solutii[i]<<" ";
-    return 0;
+int n,m,i,j,lp,lu,minp,minu, v[50][50];
+
+int main()
+
+{
+
+   cin >> n >> m;
+
+   for (i=0; i<n; ++i)
+
+       for (j=0; j<m; ++j)
+
+           cin >> v[i][j];
+
+   minp=v[0][0]; lp=0;
+
+   for (i=1; i<n; ++i)
+
+       if (v[i][0]<minp)
+
+   {
+
+       minp=v[i][0]; lp=i;
+
+   }
+
+   minu=v[0][m-1]; lu=0;
+
+   for (i=1; i<n; ++i)
+
+       if (v[i][m-1]<minu)
+
+   {
+
+       minu=v[i][m-1]; lu=i;
+
+   }
+
+   int t=v[lp][0];
+
+   v[lp][0]=v[lu][m-1];
+
+   v[lu][m-1]=t;
+
+   for (i=0; i<n; ++i)
+
+   {
+
+       for (j=0; j<m; ++j)
+
+           cout << v[i][j] << " ";
+
+       cout << "\n";
+
+   }
+
 }
