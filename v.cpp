4447ec0d513d1e997814d26beeeb3ee9199ae6a0
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 20:05:42 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,30 +1,15 @@
 #include <iostream>
-#include <queue>
+
 using namespace std;
 
-int v[10001];
+int n, i;
+int main(){
+   cin >> n;
+   cout << 1;
+   if (n>0)
+   {
+       for (i=1; i<=n; ++i)
 
-int main() {
-    int n, a, x, y;
-    cin >> n >> a >> x >> y;
-    queue <int> c;
-    c.push(a);
-    v[a] = true;
-    while(!c.empty()) {
-        int val = c.front();
-        if(val + x <= n && !v[val + x])  {
-          c.push(val + x);
-          v[val + x] = true;
-        }
-        
-        if(val + y <= n && !v[val + y]) {
-          c.push(val + y);
-          v[val + y] = true;
-        }
-        c.pop();
-    }
-    for(int i = 0; i <= n; i++)
-      if(v[i] == true)
-        cout << i <<" ";
-    return 0;
+           cout << 0;
+   }
 }
