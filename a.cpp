3ac08daa448ea95b102e4e8b4feacbfb53961108
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 15:01:34 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,16 +1,35 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
+const int dim=50; //lungimea registrului
+char avansare(char cifra){
+if(cifra==’Q’) return ’R’;
+if(cifra==’R’) return ’S’;
+if(cifra==’S’) return ’T’;
+return ’Q’; //Q este cifra zero
+}
+int incrementare(char reg[]){
+int i;
+for(i=0; i<dim; i++){
+reg[i]=avansare(reg[i]);
+if(reg[i]!=’Q’) break;
+}
+return i; //i==dim <--> registrul nul
+}
+void afisare(char reg[]){
+int i;
+for(i=dim-1;i>=0; i--)
+cout<<reg[i];
+cout<<endl;
+return;
+}
 int main(){
-   int HA, HB, D, X;
-   cin >> HA >> HB >> D;
-   if (HA==HB) X=D/2;
-   else  {
-       if (HA>HB) X=(D*D+HB*HB-HA*HA)/(2*D);
-       else X=(D*D+HA*HA-HB*HB)/(2*D);
-   }
-   if (X<0) X=-X;
-   cout << X;
-   return 0;
+char registru[dim];
+int i;
+for(i=0;i<dim; i++){ // registrul nul initial
+registru[i]=’Q’;
+}
+do{ // numararea propriu-zisa
+afisare(registru);
+}while(incrementare(registru)<dim);
+return 0;
 }
