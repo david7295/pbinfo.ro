6473c8e0041eac3f580cf1adc9c75b462ea5737d
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-06 13:35:19 +0300

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -1,43 +1,15 @@
-#include <fstream>
-#include <iostream>
+#include <cstring>
+#include <bits/stdc++.h>
 
-using namespace std;
+using namespace std;
+int nr_vocale(char s[])
+{
+    if(s[0] == NULL)  return 0;
+    int lu = strlen(s);
+    char ch = s[lu - 1]; // caracterul de pe ultima pozitie
+    s[lu - 1] = NULL; // eliminam ultimul caracter din sir
+    if(strchr("aeiouAEIOU", ch))
+        return 1 + nr_vocale(s);
 
-ifstream fin("parcare.in");
-ofstream fout("parcare.out");
-
-int z1,z2,l1,l2,h1,h2,m1,m2,t,o;
-int rz,rh,rm,rt,sum;
-int main(){
-    fin >> z1 >> l1 >> h1 >> m1;  
-    fin >> z2 >> l2 >> h2 >> m2;  
-    fin >> t;    fin >> o;
-    if(z2<z1) {     
-     if(l1==1 or l1==3 or l1==5 or l1==7 or l1==8 or l1==10 or l1==12)   
-       rz=z2+31-z1;     
-        else if(l1==4 or l1==6 or l1==9 or l1==11)        
-         rz=z2+30-z1;      
-         else if(l1==2)        
-          rz=z2+28-z1;   }   
-          else rz=z2-z1;
-    if(h2<h1)  {  
-    rz--; 
-    if(m2>=m1) rh=h2+24-h1;     
-    else if(m2<m1) rh=h2+24-h1-1;  
-    }    
-      else  {  
-      if(m2>=m1) rh=h2-h1;  
-      else if(m2<m1) rh=h2-h1-1;   
-      }
-    if(m2<m1) rm=m2+60-m1;  else if(m2>=m1) rm=m2-m1;
-    rt=rz*1440+rh*60+rm;
-    if(o==1) 
-    fout << rt;   
-    else if(o==2) { 
-    if(rt<15) fout << "0"; 
-     else { 
-     if(rt%60==0) sum=(rt/60)*t; 
-      else sum=(rt/60+1)*t; 
-      fout << sum;  }   
- }
-    return 0;}
+    return nr_vocale(s);
+}
