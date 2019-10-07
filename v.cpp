4aaa6c15cef2595d030844d026c3867a730e95d7
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-07 20:17:30 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,26 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
 int main(){
-   int n,v[100001],mst[100001],mdr[100001],aux=0,w=0;
+   int n, v[13], i, fact=1;
    cin>>n;
-   for(int i=1;i<=n;i++){
-
-       cin>>v[i];
-       if(v[i]>aux)
-           aux=v[i];
-       mst[i]=aux; }
-   aux=0;
-   for(int i=n;i>=1;i--)
-   {
-       if(v[i]>aux)
-           aux=v[i];
-       mdr[i]=aux; }
-   for(int i=2;i<n;i++)
-       if(v[i]<min(mst[i],mdr[i])) {
-       w+=(min(mst[i],mdr[i])-v[i]);
-       v[i]=min(mst[i],mdr[i]); }
-   cout<<w;
-   return 0;
+   for(i=1;i<=n;i++)
+   cin>>v[i];
+   for(i=1;i<=n;i++)
+   fact=fact*i;
+   for(i=1;i<=n;i++)
+   if(fact%v[i]==0) cout<<v[i]<<" ";
 }
