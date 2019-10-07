34099ad03a025739485708c837bd6e7341b82699
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-07 20:27:41 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,25 +1,15 @@
-#include <fstream>
+#include <iostream>
+
 using namespace std;
 
-ifstream fin("palindromsd.in");
-ofstream fout("palindromsd.out");
+short n, num, i, v[102], perechi;
 
-bool ePalindrom(int nr) {
-if(nr < 10) return true;
-int rasturnat = 0, numar = nr;
-while(numar > 0) { 
-  rasturnat = rasturnat * 10 + numar % 10; 
-  numar = numar / 10; }
-if(rasturnat == nr) return true; 
-  else return false;}
-int main() {
-int pal[1000], n = 0, nr;
-while(fin >> nr) { 
-  if(ePalindrom(nr)) pal[n++] = nr; }
-for(int i = 0; i < n; i++) { 
-  if(i % 2 == 0) { 
-    fout << pal[i / 2] << " "; } 
-  else { 
-    fout << pal[n - (i/2 + 1)] << " "; }
-}
+int main(){
+   cin >> n;
+   for (i=1; i<=n; ++i)  {
+       cin >> num; ++v[num];
+   }
+   for (i=1; i<=100; ++i)
+       perechi=perechi+v[i]/2;
+   cout << perechi;
 }
