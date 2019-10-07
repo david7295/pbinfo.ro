b21da8eae837115ef12afd7b1082488fc59f8a0f
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-07 20:19:07 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,14 +1,97 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-   int n, v[13], i, fact=1;
-   cin>>n;
-   for(i=1;i<=n;i++)
-   cin>>v[i];
-   for(i=1;i<=n;i++)
-   fact=fact*i;
-   for(i=1;i<=n;i++)
-   if(fact%v[i]==0) cout<<v[i]<<" ";
+int main()
+
+{
+
+   unsigned long long nr,v[100],i=1,t=0;
+
+   cin >> nr;
+
+   int k;
+
+   while(nr !=0)
+
+   {
+
+       v[i] = nr % 10;
+
+       nr = nr / 10;
+
+       i++;
+
+   }
+
+   for(int k=1; k<=i-1; k++)
+
+       {
+
+           nr = v[k]* 2;
+
+           v[k] = nr % 10 + t;
+
+           if(nr > 9)
+
+               t = 1;
+
+           else
+
+               t = 0;
+
+       }
+
+   if(t==1)
+
+       v[i] = t;
+
+   else
+
+       i--;
+
+   t = 0;
+
+/*    for(int j=1; j<=i; j++)
+
+   {
+
+       if(v[j] == 0) {
+
+           v[j] = 9;
+
+       t = 1;
+
+       }
+
+       else if(j == 1)
+
+           v[j]--;
+
+    else   {
+
+           v[j] -=t ;
+
+           t = 0;
+
+       }
+
+   } */
+
+   for(int k=i; k>=1; k--)
+
+   {
+
+       if(v[k]!=0)
+
+           t =1 ;
+
+       if(t == 1)
+
+           cout << v[k];
+
+   }
+
+   return 0;
+
 }
