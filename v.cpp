cdd5ce00c97fbfec0890a93ce8987384424015a0
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-07 19:38:57 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
-
 using namespace std;
-
-int a[101][101];
-
-int main(){
-   int n, m, i, j, c=0, egale;
-   cin >> n >> m;
-   for (i=1; i<=n; ++i)
-       for (j=1; j<=m; ++j)
-           cin >> a[i][j];
-   if (n==1) c=0;
-   else {
-       for (i=2; i<=n; ++i)  {
-           egale=1;
-            for (j=1; j<=m && egale; ++j)
-               if (a[i][j]!=a[i-1][j]) egale=0;
-           if (egale) ++c;
-       }
-   }
-   cout << c;
-   return 0;
+int main()
+{
+    int n,m,v[100][100],C=0;
+    cin>>n>>m;
+     for(int i=1;i<=n;i++)
+     for(int j=1;j<=m;j++)
+     {
+       cout<<"v["<<i<<"]["<<j<<"]=";
+       cin>>v[i][j];
+     }
+   for(int i=1;i<n;i++)
+     for(int j=i+1;j<=n;j++)
+     {    
+         bool ok=true;
+         int k=1;
+         while(k<=m && ok)
+         {
+             if(v[i][k]!=v[j][k] )
+                ok=false;
+             else
+                k++;
+         }
+         if(ok)
+            C++;
+     }
+     cout<<C;
+     return 0;
 }
