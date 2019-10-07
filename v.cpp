fbdd32f57b8404130ec617d8939a12a7ff11225b
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-07 20:52:55 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,17 +1,24 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
-using namespace std;
-int main()
-{    char char s[201];
-     int i;
-     ifstream in("prosir.in");
-     ofstream out("prosir.out");
-     in.get(s,201);
-     for(i=1;s[i];i++)
-       if(s[i]==' ' and isalpha(s[i-1]))
-         s[i-1]='5';
-     s[strlen(s)-2]='5';
-     cout<<s;
-     return 0;
+#include <bits/stdc++.h>
+using namespace std;
+int n, i, k, nr, lu;
+char s[256], c[25], *p;
+string v[256];
+int main()
+{
+   ifstream f("prosir.in");
+   ofstream g("prosir.out");
+   f.getline(s, sizeof(s));
+   p = strtok(s, " .");
+   while(p)
+{
+strcpy(c, p);
+lu = strlen(c);
+c[lu - 1] = '5';
+v[++ k] = c;
+p = strtok(NULL, " .");
+}
+for(i = 1; i < k; i ++)
+ g << v[i] << " ";
+g << v[k] << ".";
+    return 0;
 }
