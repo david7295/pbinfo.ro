842989c6f3ac26e283ef3113a86dd9bf30993ab6
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-08 22:11:15 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,21 +1,18 @@
-#include <iostream>
- using namespace std;
-  int main()
- {
-int n, c, produs = 1, ok;
-cin >> n;
-ok=false;
- while (n)
- {
- c = n % 10;
- if ( c % 2 == 1 )
- {
-          produs = produs * c;
-          ok=true;
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int numar, nr_cifre = 0;
+
+cin >> numar;
+
+while(numar != 0) {
+numar /= 10;
+nr_cifre++;
 }
- n /= 10;
- }
- if (ok) cout << produs;
-        else cout << -1;
- return 0;
- }
+
+cout << nr_cifre << '\n';
+
+return 0;
+}
