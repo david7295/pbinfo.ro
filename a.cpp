4fd196c0795ed90f75ee513a96c6bbea07e27b1f
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-08 22:40:05 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,16 +1,37 @@
-#include <iostream>
-using namespace std;
+#include <fstream>
+#include <cstring>
 
-int main(){
-int n, ucp=-1;
-cin >> n;
-do
+using namespace std;
+
+ifstream fin("palindrom.in");
+ofstream fout("palindrom.out");
+
+int n;
+char s[200],s1[200];
+
+int main()
 {
-if(n%2==0)
-ucp = n%10, n=0;
-n /=10;
-}
-while(n);
-cout << ucp;
-return 0;
+
+    fin >> n;
+    fin.get();
+
+    for(;n;n--)
+    {
+        fin.getline(s,200);
+        int nr=0,ok=1;
+        s1[0]='\0';
+        for(int i=0;i<strlen(s);i++)
+            if(!isspace(s[i]))
+                s1[nr++]=s[i];
+        for(int i=0;i<nr;i++)
+            if(s1[i]!=s1[nr-1-i])
+                ok=0;
+        if(ok)
+            fout << 1 << endl;
+        else
+            fout << 0 << endl;
+    }
+
+    return 0;
+
 }
