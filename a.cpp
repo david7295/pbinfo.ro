99b1e47d9a0396e3fe3188cc7f2443cb523343cc
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-08 21:56:05 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,26 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-int n, i, x, j, a, b;
-bool prim;
-double s, ma, k;
-int main()
+#include <iostream>
+using namespace std;
+
+int main()
+
 {
-    cin >> a >> b;
-    if(a > b) swap(a, b);
-    for (i = a; i <= b; i ++)
-    {
-        x = i;
-        prim = true;
-        if(x == 0 || x == 1) prim = false;
-        for (j = 2; j * j <= x; j++)
-            if (x % j == 0)
-        {
-            prim = false;
-            break;
-        }
-        if (prim)
-           k ++;
-    }
-    cout << k;
-    return 0;
+
+    int n, perechi=0, ucp, ucc;
+
+    cin >> n;
+
+    if (n<0) n=-n;
+
+    ucp = n % 10;
+
+    while (n)
+
+    {
+
+        cin >> n;
+
+        if (n)
+
+        {
+
+          if (n<0) n=-n;
+
+          ucc = n % 10;
+
+          if (ucp==ucc) ++ perechi;
+
+          ucp=ucc;
+
+        }
+
+
+
+    }
+
+    cout << perechi << endl;
+
+    return 0;
+
 }
