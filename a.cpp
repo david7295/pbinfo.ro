abf7bc904258c0981f2b73ab38885cde1cdc3682
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-08 20:37:22 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,44 +1,26 @@
-#include <iostream>
-#define Nmax 100005
-using namespace std;
-int a[Nmax], n, k;
+#include <iostream>
+#include <fstream>
 
-void poz(int li, int ls, int &k, int a[])
-{
-    int i = li, j = ls, c, i1 = 0, j1 = -1;
-    while(i < j)
-    {
-        if(a[i] > a[j])
-        {
-            c = a[j];
-            a[j] = a[i];
-            a[i] = c;
-            c = i1;
-            i1 = - j1;
-            j1 = - c;
-        }
-        i = i + i1;
-        j = j + j1;
-    }
-    k = i;
+using namespace std;
+
+ifstream fin("clase.in");
+ofstream fout("clase.out");
+
+int nr,n,m,i,j,a,b;
+
+int main(){
+
+cin>>n>>m;
+
+for(i=1;i<=n;i++){
+
+cin>>a;
+for(j=1;j<m;j++)
+ cin>>b;
+if(a==b) nr++
 }
-void quick(int li, int ls)
-{
-    if (li < ls)
-    {
-        poz(li, ls, k, a);
-        quick(li, k - 1);
-        quick(k + 1, ls);
-    }
-}
-int main()
-{
-    int i;
-    cin >> n;
-    for(i = 1; i <= n; i ++)
-        cin >> a[i];
-    quick(1, n);
-    for(i = 1; i <= n; i ++)
-        cout << a[i] << " ";
-    return 0;
+
+cout<<nr;
+   return 0;
+
 }
