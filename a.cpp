831640f6f106b3568b222d0eae3bc11c2838cdff
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-08 20:55:19 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
-
 using namespace std;
-
-short c[1001], i, n, pal;
-
-int main() {
-
-  cin >> n;
-
-  for (i=0; i<n; ++i)
-
-  {
-
-      cin >> c[i]; 
-  }
-  for (i=0; i<n; ++i)
-     cout << c[i];
-  cout << "\n";
-  pal=1;
-  for (i=0; i<n/2; ++i)
-       if (c[i]!=c[n-i-1]) { pal=0; break; }
-  if (pal) cout << "DA";
-  else cout << "NU";
+int main()
+{
+  int v,n,i,ok=1;
+cout<<"n=";
+  cin>>n;
+  int a[n+1];
+cout<<"introduceti sirul "<<endl;
+  cin>>a[1];
+  for(i=2;i<=n;i++)
+    cin>>a[i];
+  for(i=2;i<=n;i++)
+  if(a[i-1]>a[i]&&a[i+1]>a[i])
+        v=i;
+  for(i=1;i<v;i++)
+    if(a[i+1]>=a[i])
+    {
+    cout<<"NU"; ok=0;
+    break;
+    }
+  if(ok)
+    for(i=v;i<n;i++)
+      if(a[i]>=a[i+1])
+      {
+      cout<<"NU"; ok=0;
+      break;
+      }
+  if(ok)
+        cout<<"DA";
+        return 0;
 }
