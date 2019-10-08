969a1627a170d6592387a7ecd3049eb1818cd380
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-08 21:28:52 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,30 +1,41 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-int v[105],v1[105],n;
-int main()
-{
-    int i,da=1,j;
-    cin >> n;
-    for(i=0;i<n;i++)
-        cin >> v[i];
-    for(i=0;i<n;i++)
-        cin >> v1[i];
-    for(i=0;i<n;i++)
-        for(j=i+1;j<=n;j++)
-            if(v[j]>v[i])
-                swap(v[i],v[j]);
-    for(i=0;i<n;i++)
-        for(j=i+1;j<=n;j++)
-            if(v1[j]>v1[i])
-                swap(v1[i], v1[j]);
-    for(i=1;i<n;i++)
-        if(v[i-1]*v1[n-i]!= v[i]*v1[n-i-1]) // i.p
-            da=0;
-    if(da)
-        cout << "DA";
-    else
-        cout << "NU";
-    return 0;
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main()
+{
+  int v[1000];
+  long int i,j,n,ma=-11110,mi=100000,poz1,poz2;
+  cin>>n;
+  for(i=1;i<=n;i++)
+    {
+        cin>>v[i];
+        if(v[i]>ma)
+        {
+            ma=v[i];
+            poz1=i;
+        }
+        if(v[i]<mi)
+        {
+            mi=v[i];
+            poz2=i;
+        }
+    }
+    if(poz1>poz2)
+    {
+        for(i=poz2;i<=poz1-1;i++)
+            for(j=i+1;j<=poz1;j++)
+                if(v[i]>v[j])
+                    swap(v[i],v[j]);
+    }
+    else
+    {
+        for(i=poz1;i<=poz2-1;i++)
+            for(j=i+1;j<=poz2;j++)
+                if(v[i]>v[j])
+                    swap(v[i],v[j]);
+    }
+  for(i=1;i<=n;i++)
+       cout<<v[i]<<' ';
+  return 0;
 }
