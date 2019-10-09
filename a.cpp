4c6125109fe568533416414983e6343b11c3d401
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-09 20:46:08 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,20 +1,65 @@
 #include <iostream>
+
 using namespace std;
-int main(){
-int max=0;
-int n;
-cin>>n;
- while(n!=0)
-{ if ((n%10>max) && ((n%10)%2!=0)){
-max=n%10;
-}
-n=n/10;
-}
-if (max==0) {
-cout<<"Nu exista";
-}
-else{
-cout<<max;
-}
-  return 0;
+
+int main()
+
+{  int a,b,a1,b1,k=0,nrc1=0,nrc2=0;
+
+   cin>>a>>b;//citim a si b
+
+   a1=a;
+
+   b1=b;//copiem a si b in 2 variabile
+
+   while(a1)
+
+   {
+
+        if(a1>0)
+
+         nrc1++;
+
+       a1=a1/10; // aflam cate cifre are a
+
+   }
+
+   while(b1)
+
+   {
+
+       if(b1>0)
+
+         nrc2++;
+
+      b1=b1/10;//aflam cate cifre are b
+
+   }
+
+   if(nrc1!=nrc2)//verificam daca au acelasi nr de cifre
+
+    cout<<"NU";
+
+   else
+
+       {
+
+
+        while(a && b)
+
+        { if(a%10==b%10)//aflam cate cifre identice se afla pe aceleasi pozitii
+
+            k++;
+
+            a=a/10;
+
+            b=b/10;
+
+        }
+
+        cout<<k;
+
+   }
+
+
 }
