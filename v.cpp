73a84a81e37877ab640929c3411de9173e96ddaf
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-14 18:25:10 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -2,26 +2,42 @@
 
 using namespace std;
 
-long long n,p,f;
+short n, m, i, j;
 
 int main()
 
 {
 
-   cin >> n >> p;
+  cin >> n >> m;
 
-   f=n;
+  for (j=1; j<=m; ++j)
 
-   while (f<p)
+     cout << "__0_0___";
 
-   {
+  cout << endl;
 
-      cout << f << " ";
+  if (n>1)
 
-      f=f*n;
+  {
 
-   }
+      for (i=2; i<n; ++i)
 
-   if (f==p) cout << f;
+       {
+
+           for (j=1; j<=m; ++j)
+
+               cout << "_0___0__";
+
+           cout << endl;
+
+       }
+
+       for (j=1; j<=m; ++j)
+
+           cout << "0_____0_";
+
+       cout << endl;
+
+  }
 
 }
