3ce8b53adf8f49bee366d1fdfccc483553345f28
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-14 18:11:17 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
-#include <cstring>
-
 using namespace std;
-
-int main(){
-   char s[101];
-
-   cin>>s;
-
-   int i, n, OK = 1;  // considerăm că șirul conține doar consoane
-
-   n=strlen(s)   
-
-   for (i = 0; i <= strlen(s); i ++)
-
-          {  if( strchr ( "aeou", s[i] ) == 1 )  // dacă găsim vocală ( fără i )
-
-                     OK=0; // OK e zero deci am găsit vocala și urmează "NU"
-
-            if( OK == 1)
-                    cout << " DA ";
-             else
-                    cout<< " NU "; }
-return 0; 
+int n, p, nr;
+int main()
+{
+    cin >> n >> p;
+    nr = 1;
+    while(nr <= p)
+    {
+        cout << nr << " ";
+        nr = nr * n;
+    }
+    return 0;
 }
