4faba77753d6d65c75ca2c0c3ab734c59288c01e
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-14 19:51:11 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,65 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <bitset>
 
 using namespace std;
 
+bitset<1000000>a;
+
 int main()
 
-{  int a,b,a1,b1,k=0,nrc1=0,nrc2=0;
+{
 
-   cin>>a>>b;//citim a si b
+   ifstream f("interclasm.in");
 
-   a1=a;
+   ofstream g("interclasm.out");
 
-   b1=b;//copiem a si b in 2 variabile
+   int x, n, m, i, num;
 
-   while(a1)
+   f>>x>>n;
 
-   {
-
-        if(a1>0)
-
-         nrc1++;
-
-       a1=a1/10; // aflam cate cifre are a
-
-   }
-
-   while(b1)
+   for (i=1; i<=n; ++i)
 
    {
 
-       if(b1>0)
-
-         nrc2++;
-
-      b1=b1/10;//aflam cate cifre are b
+       f >> num; a[num]=1;
 
    }
 
-   if(nrc1!=nrc2)//verificam daca au acelasi nr de cifre
+   f >> m;
 
-    cout<<"NU";
+   for (i=1; i<=m; ++i)
 
-   else
+   {
 
-       {
+       f >> num;
 
+       if (a[num]) a[num]=0;
 
-        while(a && b)
-
-        { if(a%10==b%10)//aflam cate cifre identice se afla pe aceleasi pozitii
-
-            k++;
-
-            a=a/10;
-
-            b=b/10;
-
-        }
-
-        cout<<k;
+       else a[num]=1;
 
    }
 
+   for (i=1; i<1000000; ++i)
+
+       if (a[i] && i%x==0)
+
+           g << i << " ";
 
 }
