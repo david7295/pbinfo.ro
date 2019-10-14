472221671097c07dfa97dbd4db9ba768df1136da
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-14 20:18:38 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,49 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include <bitset>
-
 using namespace std;
 
-bitset<1000000>a;
+ifstream in ("restmare.in");
+ofstream out ("restmare.out");
 
-int main()
-
-{
-
-   ifstream f("interclasm.in");
-
-   ofstream g("interclasm.out");
-
-   int x, n, m, i, num;
-
-   f>>x>>n;
-
-   for (i=1; i<=n; ++i)
-
-   {
-
-       f >> num; a[num]=1;
-
-   }
-
-   f >> m;
-
-   for (i=1; i<=m; ++i)
-
-   {
-
-       f >> num;
-
-       if (a[num]) a[num]=0;
-
-       else a[num]=1;
-
-   }
-
-   for (i=1; i<1000000; ++i)
-
-       if (a[i] && i%x==0)
-
-           g << i << " ";
-
+long long int n,pana_unde,suma;
+int main() {
+  in >> n;
+    if(n>2)    {
+   if(n%2==1)  {   
+     pana_unde = n/2;    
+     suma = (pana_unde*(pana_unde+1))/2 + ((pana_unde-1)*pana_unde)/2;    }   
+    else {    
+      pana_unde = n/2-1;     
+      suma = pana_unde*(pana_unde+1);  
+    }
+ }
+    out << suma;
+    return 0;
 }
