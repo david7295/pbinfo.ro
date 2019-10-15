dede0e3ffb61ec7e774e0acaf28a28207726eb5e
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 20:25:04 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,23 +1,65 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
-{
-    int r,k=0,n,j,S;
-    char s[20];
-    cin>>n>>r;
-    for(int i=0;i<n;i++)
-    {
-        cin>>s;
-        S=0;
-        j=0;
-        while(s[j]>='0' && s[j]<='9')
-        {
-            S=S+(s[j]-48);
-            j++;
-        }
-         if(S%9==r)
-             k=k+1;
-    }
-    cout<<k;
-     return 0;
+
+{  int a,b,a1,b1,k=0,nrc1=0,nrc2=0;
+
+   cin>>a>>b;//citim a si b
+
+   a1=a;
+
+   b1=b;//copiem a si b in 2 variabile
+
+   while(a1)
+
+   {
+
+        if(a1>0)
+
+         nrc1++;
+
+       a1=a1/10; // aflam cate cifre are a
+
+   }
+
+   while(b1)
+
+   {
+
+       if(b1>0)
+
+         nrc2++;
+
+      b1=b1/10;//aflam cate cifre are b
+
+   }
+
+   if(nrc1!=nrc2)//verificam daca au acelasi nr de cifre
+
+    cout<<"NU";
+
+   else
+
+       {
+
+
+        while(a && b)
+
+        { if(a%10==b%10)//aflam cate cifre identice se afla pe aceleasi pozitii
+
+            k++;
+
+            a=a/10;
+
+            b=b/10;
+
+        }
+
+        cout<<k;
+
+   }
+
+
 }
