fb82bfe698f78cfb3a1d36189d0065737708f74d
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 07:20:58 +0300

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,63 +1,24 @@
-#include <iostream>
+#include <iostream>
 
-using namespace std;
+using namespace std;
 
-int main()
+int n,a,b,p;
 
+int main()
 {
 
-   int n,k,r,line,sk, sr, primulk, ultimulk, cardpar, primulr,ultimulr;
+    cin >> n;
 
-   cin >> n >> k >> r;
+    for(int i=1;i<=n;i++)
+    {
+        cin >> a >> b;
+        p=1;
+        while(p*2<=max(a,b))
+            p*=2;
+        if(p>=min(a,b)) cout << p << endl;
+        else cout << 0 << endl;
+    }
 
-   cardpar=n/2;
-
-   if (k%2==0) primulk=(k/2)*(k-1);
-
-   else primulk=((k-1)/2)*k;
-
-   ultimulk=(k-1)+primulk;
-
-   if (k%2==0)
-
-       sk=(k/2)*(primulk+ultimulk);
-
-   else sk=((primulk+ultimulk)/2)*k;
-
-   if (r%2==0) primulr=(r/2)*(r-1);
-
-   else primulr=((r-1)/2)*r;
-
-    ultimulr=(r-1)+primulr;
-
-   if (r%2==0)
-
-       sr=(r/2)*(primulr+ultimulr);
-
-   else sr=((primulr+ultimulr)/2)*r;
-
-   cout << cardpar << endl;
-
-   cout << (sk+sr)/(k+r) << endl;
-
-   while (primulr<ultimulr)
-
-   {
-
-       cout << primulr << " ";
-
-       ++primulr;
-
-       if (primulr<ultimulr)
-
-           cout << primulr << " ";
-
-       cout << ultimulr << " ";
-
-       ++primulr; --ultimulr;
-
-   }
-
-   if (primulr==ultimulr) cout << primulr;
+    return 0;
 
 }
