4ddef0c1b1b9422f1e7b3f2fdf0c6154c2d376b2
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 20:21:03 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
-
-int main(){
- 
- int n,k,p=1,c;
-cin>>n>>k;
-
-while(n!=0){
- c=n%10;
-
-if(c!=k)
- p=p*c;
-n=n/10;}
-cout<<p;
-return 0;
+int main()
+{
+    int r,k=0,n,j,S;
+    char s[20];
+    cin>>n>>r;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s;
+        S=0;
+        j=0;
+        while(s[j]>='0' && s[j]<='9')
+        {
+            S=S+(s[j]-48);
+            j++;
+        }
+         if(S%9==r)
+             k=k+1;
+    }
+    cout<<k;
+     return 0;
 }
