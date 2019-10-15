5df1cfec523aeafc8bedbae8788d538afef01155
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 20:01:25 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,129 +1,62 @@
 #include <iostream>
-#include <cstring>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
-ifstream f("inversmax.in");
-
-bool litera(char c)
-
-{
-
-   if ((c>='A' && c<='Z')||(c>='a' && c<='z')) return 1;
-
-   else return 0;
-
-}
-
-char s[256], w[256], ch;
-
-int n, i, j, lcuv, ok, ind, lmax, poz[200], start;
+ifstream fin("paritatesiruri.in");
+ofstream fout("paritatesiruri.out");
 
 int main()
 
 {
 
-   f.getline(s,256);
+   int K, s, i, p;
 
-   n=strlen(s);
+   char sir[255], sir1[255], sir2[255];
 
-   s[n]=' '; ++n; s[n]=0;
+   fin>>K;
 
-   i=0;
-
-   while (i<n)
+   for(s=1;s<=K;s++)
 
    {
 
-       if (s[i]!=' ')
+       fin>>sir;
+
+       sir1[0]='\0'; sir2[0]='\0';
+
+       p=0;
+
+       for(i=1;i<strlen(sir);i=i+2)
 
        {
 
-           ++lcuv;
-
-           if (litera(s[i])) ok=1;
+           sir1[p]=sir[i]; p++;
 
        }
 
-       else
+       sir1[p]='\0';
+
+       p=0;
+
+       for(i=0;i<strlen(sir);i=i+2)
 
        {
 
-           if(ok)
-
-           {
-
-               if (lcuv>lmax)
-
-               {
-
-                   lmax=lcuv;
-
-                   start=i-lmax;
-
-                   ind=1; poz[ind]=start;
-
-               }
-
-               else
-
-               {
-
-                   if (lcuv==lmax)
-
-                   {
-
-                       ++ind; poz[ind]=i-lmax;
-
-                   }
-
-               }
-
-           }
-
-           lcuv=0; ok=0;
+           sir2[p]=sir[i]; p++;
 
        }
 
-       ++i;
+       sir2[p]='\0';
+
+       fout<<sir1<<' '<<sir2<<'\n';
 
    }
 
-   i=0;  ind=1; j=poz[ind];
+   fin.close();
 
-   while (i<n)
+   fout.close();
 
-   {
-
-       if (i==j)
-
-       {
-
-           for (int p=i; p<i+lmax; ++p)
-
-               w[p]=s[i+lmax+i-p-1];
-
-           i=i+lmax-1; ++ind;
-
-           j=poz[ind];
-
-       }
-
-       else
-
-       {
-
-           w[i]=s[i];
-
-       }
-
-       ++i;
-
-   }
-
-   --n; w[n]=0;
-
-   cout << w;
+   return 0;
 
 }
