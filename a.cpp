93f4fb35ed1e185ce62e3b8e4996669fd054352d
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 18:30:07 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,25 +1,27 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
-
-int main()
+#include <iostream>
+#include <cstring>
+using namespace std;
+char s[257];
+int i,va,ve,vi,vo,vu,m;
+int main()
 {
-    char s[100];
-    cin>>s;
-    int i,ok=1,k=0;
-    for(i=0;i<strlen(s);i++)
-    {
-        if(s[i]=='i')
-                k++;
-        if(strchr("aeou",s[i]))
-        {
-        ok=0;
-        break;
-        }
-    }
-    if(ok && k!=strlen(s))
-        cout<<"DA";
-    else
-        cout<<"NU";
-return 0;        
+    cin.get(s,257);
+    for(i=0;i<strlen(s);i++)
+    {
+        if(strchr("Aa",s[i])!=NULL) va++;
+        else if(strchr("Ee",s[i])!=NULL) ve++;
+        else if(strchr("Ii",s[i])!=NULL) vi++;
+        else if(strchr("Oo",s[i])!=NULL) vo++;
+        else if(strchr("Uu",s[i])!=NULL) vu++;
+    }
+    m=va;
+    if(ve>m) m=ve;
+    if(vi>m) m=vi;
+    if(vo>m) m=vo;
+    if(vu>m) m=vu;
+    if(va==m) cout<<"A";
+    else if(ve==m) cout<<"E";
+    else if(vi==m) cout<<"I";
+    else if(vo==m) cout<<"O";
+    else cout<<"U";
 }
