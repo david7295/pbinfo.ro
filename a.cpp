e37f85831cdb17fea4ce8f305b251624767f4873
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 20:02:57 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,62 +1,47 @@
 #include <iostream>
-#include <fstream>
-#include <string.h>
+
+#include <cstring>
 
 using namespace std;
 
-ifstream fin("paritatesiruri.in");
-ofstream fout("paritatesiruri.out");
+char s[256];
+
+long long suma=0, termen, i, lung;
 
 int main()
 
 {
 
-   int K, s, i, p;
+    cin.getline(s, 256);
 
-   char sir[255], sir1[255], sir2[255];
+    lung=strlen(s);
 
-   fin>>K;
+    for (i=0; i<lung; ++i)
 
-   for(s=1;s<=K;s++)
+    {
 
-   {
+        if (isdigit(s[i]))
 
-       fin>>sir;
+        {
 
-       sir1[0]='\0'; sir2[0]='\0';
+            termen=termen*10+(s[i]-'0');
 
-       p=0;
+        }
 
-       for(i=1;i<strlen(sir);i=i+2)
+        else
 
-       {
+        {
 
-           sir1[p]=sir[i]; p++;
+            suma=suma+termen; termen=0;
 
-       }
+        }
 
-       sir1[p]='\0';
+    }
 
-       p=0;
 
-       for(i=0;i<strlen(sir);i=i+2)
 
-       {
+    cout << suma;
 
-           sir2[p]=sir[i]; p++;
-
-       }
-
-       sir2[p]='\0';
-
-       fout<<sir1<<' '<<sir2<<'\n';
-
-   }
-
-   fin.close();
-
-   fout.close();
-
-   return 0;
+    return 0;
 
 }
