74b0a432341b8a4baf4f4d140513c5294cdd9c68
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-15 18:27:54 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,55 +1,25 @@
-#include <iostream>
+#include <iostream>
+#include <cstring>
+using namespace std;
 
-#include <algorithm>
-
-using namespace std;
-
-int T,i,x,j,nr_greu=0,nr_usor=0,y,a;
-
-int main()
-
+int main()
 {
-
-   cin>>T;
-
-   i=1;
-
-   while(i<=T)
-
-   {
-
-       cin>>x>>y;
-
-       for(int j=1;j<=x;j++)
-
-       {
-
-           cin>>a;
-
-           if(a>=y/2)
-
-               nr_usor++;
-
-           if(a<=y/10)
-
-               nr_greu++;
-
-       }
-
-       if(nr_usor==1 && nr_greu==2)
-
-           cout<<"da"<<endl;
-
-       else
-
-           cout<<"nu"<<endl;
-
-       nr_usor=0;
-
-       nr_greu=0;
-
-       i++;
-
-   }
-
+    char s[100];
+    cin>>s;
+    int i,ok=1,k=0;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(s[i]=='i')
+                k++;
+        if(strchr("aeou",s[i]))
+        {
+        ok=0;
+        break;
+        }
+    }
+    if(ok && k!=strlen(s))
+        cout<<"DA";
+    else
+        cout<<"NU";
+return 0;        
 }
