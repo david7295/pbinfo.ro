feb49b514d25dcc4a2a54454dbdab62e932f6027
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-17 15:26:17 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,41 +1,20 @@
 #include <iostream>
-
-using namespace std;
-
+ using namespace std;
+int n,m,Max,i,j,v[101],a[101][101];
 int main()
 {
-int mat[101][101],n,m,i,j,maxim,k,y,t;
-cin>>n>>m;
-for(i=1;i<=n;i++)
-for(j=1;j<=m;j++)
-cin>>mat[i][j];
-for(i=1;i<=n;i++)
-{
-for(j=1;j<=m;j++)
-{
-maxim=mat[i][j];
-k=0;
-for(y=1;y<=n;y++)
-for(t=1;t<=m;t++)
-if(mat[y][t]>maxim)
-{
-maxim=mat[y][t];
-k=k+1;
-}
-if(k>=2)
-{
-cout<<maxim;
-return 0;
-}
-else
-{
-for(y=1;y<=n;y++)
-for(t=1;t<=m;t++)
-if(mat[y][t]==maxim)
-mat[y][t]=-1000001;
-}
-}
-}
-cout<<"IMPOSIBIL";
-return 0;
+    cin>>n>>m;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=m;j++)
+        {
+            cin>>a[i][j];
+            if(a[i][j]%2==0)
+                v[i]++;
+        }
+        if(v[i]>Max) Max=v[i];
+    }
+    for(i=1;i<=n;i++)
+        if(v[i]==Max) cout<<i<<" ";
+    return 0;
 }
