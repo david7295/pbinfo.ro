2b1c4acce110c862797ef9f35717c366c34963d7
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-17 15:19:15 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
-#include <cmath> 
+
 using namespace std;
-int main() {
-  long int a[102][102],n,m,i,j,k=0; 
-  cin>>n>>m;
-  for(i=1;i<=n;i++) { 
-    a[i][0]=0;
-    a[i][m+1]=0; 
-  } 
-  for(i=1;i<=m;i++) {
-    a[0][i]=0;
-    a[n+1][i]=0; 
-   } 
-  for(i=1;i<=n;i++) {
-    for(j=1;j<=m;j++) 
-      cin>>a[i][j]; 
-         }
-  for(i=1;i<=n;i++) 
-    for(j=1;j<=m;j++) 
-      if(a[i-1][j]%2==0 && a[i+1][j]%2==0 && a[i][j-1]%2==0 && a[i][j+1]%2==0)
-        k++; 
-       cout<<k;
-  return 0; 
+
+int main()
+{
+int mat[101][101],n,m,i,j,maxim,k,y,t;
+cin>>n>>m;
+for(i=1;i<=n;i++)
+for(j=1;j<=m;j++)
+cin>>mat[i][j];
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=m;j++)
+{
+maxim=mat[i][j];
+k=0;
+for(y=1;y<=n;y++)
+for(t=1;t<=m;t++)
+if(mat[y][t]>maxim)
+{
+maxim=mat[y][t];
+k=k+1;
+}
+if(k>=2)
+{
+cout<<maxim;
+return 0;
+}
+else
+{
+for(y=1;y<=n;y++)
+for(t=1;t<=m;t++)
+if(mat[y][t]==maxim)
+mat[y][t]=-1000001;
+}
+}
+}
+cout<<"IMPOSIBIL";
+return 0;
 }
