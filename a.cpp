a1429a7dc987bff967da13b712f759598a068a48
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-17 15:29:43 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,20 +1,63 @@
 #include <iostream>
- using namespace std;
-int n,m,Max,i,j,v[101],a[101][101];
+
+using namespace std;
+
+int a[101][101];
+
 int main()
+
 {
-    cin>>n>>m;
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=m;j++)
-        {
-            cin>>a[i][j];
-            if(a[i][j]%2==0)
-                v[i]++;
-        }
-        if(v[i]>Max) Max=v[i];
-    }
-    for(i=1;i<=n;i++)
-        if(v[i]==Max) cout<<i<<" ";
-    return 0;
+
+   int n, m, i, j, t, schimb;
+
+   cin >> n >> m;
+
+   for (i=1; i<=n; ++i)
+
+       for (j=1; j<=m; ++j)
+
+           cin >> a[i][j];
+
+   do
+
+   {
+
+       schimb=0;
+
+       for (j=1; j<m; ++j)
+
+       if (a[1][j]>a[1][j+1])
+
+           {
+
+              schimb=1;
+
+              for (i=1; i<=n; ++i)
+
+              {
+
+                t=a[i][j]; a[i][j]=a[i][j+1]; a[i][j+1]=t;
+
+              }
+
+           }
+
+   }
+
+   while (schimb);
+
+   for (i=1; i<=n; ++i)
+
+   {
+
+       for (j=1; j<=m; ++j)
+
+           cout << a[i][j] << " ";
+
+       cout << "\n";
+
+   }
+
+   return 0;
+
 }
