13ba69f9061dab10fd31b9738e0c524e4884d2e4
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-17 15:39:37 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,65 +1,26 @@
-#include <iostream>
+#include <iostream>
 
-#include <fstream>
-
-using namespace std;
-
-int a[101][101];
-
-int main()
-
+using namespace std;
+int v[1005], s, n, a, b, i;
+int main()
 {
-
-   ifstream f("matrice7.in");
-
-   ofstream g ("matrice7.out");
-
-   int n, m, i, j, max;
-
-   f >> n >> m;
-
-   max=-1;
-
-    for (i=1; i<=n; ++i)
-
-       for (j=1; j<=m; ++j)
-
-           { f >> a[i][j]; if (a[i][j]>max) max=a[i][j];}
-
-   int mincol[101], min;
-
-   for (j=1; j<=m; ++j)
-
-   {
-
-       min=a[1][j];
-
-       for (i=2; i<=n; ++i)
-
-           if (a[i][j]<min) min=a[i][j];
-
-       mincol[j]=min;
-
-   }
-
-   for (i=1; i<=n; ++i)
-
-       for (j=1; j<=m; ++j)
-
-           if (a[i][j]==max) a[i][j]=mincol[j];
-
-   for (i=1; i<=n; ++i)
-
-   {
-
-       for (j=1; j<=m; ++j)
-
-           g << a[i][j] << " ";
-
-       g << "\n";
-
-   }
-
-   return 0;
-
+    cin >> n;
+    for(i = 1; i <= n;i ++)
+    {
+        cin >> v[i];
+        if(v[i] % 2 == 1)
+        b = i;
+    }
+    for(i = 1; i <= n; i ++)
+    {
+        if(v[i] % 2 == 1)
+               {
+                   a = i;
+                   break;
+               }
+    }
+    for(i = a; i <= b; i ++)
+          s = s + v[i];
+    cout << s;
+    return 0;
 }
