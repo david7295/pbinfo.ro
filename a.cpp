62cf8b2f8c2259f28b21835a2387b51fcb03fd9e
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-17 15:16:24 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
-
+#include <cmath> 
 using namespace std;
-
-int a[51][51], n, m, i ,j;
-
-int main()
-
-{
-
-   cin >> n >> m;
-
-   for (i=0; i<n; ++i)
-
-   {
-
-       for (j=0; j<m; ++j)
-
-           cin >> a[i][j];
-
-   }
-
-   int t=a[n-1][m-1];
-
-   for (i=0; i<n; ++i)
-
-   {
-
-       for (j=0; j<m; ++j)
-
-       {
-
-           if (i==0 || i==n-1 || j==0 || j==m-1)
-
-               a[i][j]=t;
-
-           cout << a[i][j] << " ";
-
-       }
-
-       cout << endl;
-
-   }
-
+int main() {
+  long int a[102][102],n,m,i,j,k=0; 
+  cin>>n>>m;
+  for(i=1;i<=n;i++) { 
+    a[i][0]=0;
+    a[i][m+1]=0; 
+  } 
+  for(i=1;i<=m;i++) {
+    a[0][i]=0;
+    a[n+1][i]=0; 
+   } 
+  for(i=1;i<=n;i++) {
+    for(j=1;j<=m;j++) 
+      cin>>a[i][j]; 
+         }
+  for(i=1;i<=n;i++) 
+    for(j=1;j<=m;j++) 
+      if(a[i-1][j]%2==0 && a[i+1][j]%2==0 && a[i][j-1]%2==0 && a[i][j+1]%2==0)
+        k++; 
+       cout<<k;
+  return 0; 
 }
