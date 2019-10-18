1aa6a393aa6b55a872ba79b811009087faa2280c
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-18 17:19:12 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,19 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
-
 {
-
-   int ip,in,cif;
-
-   double x=5*sqrtl(2)-7,n,p,pp;
-
-   cin>>n;
-
-   for(int i=1;i<=n;i++)
-
-       cout<<0;
-
+    int p, q, n, nrP=0, nrQ=0;
+    cin >> p >> q >> n;
+    for (int i=0; i<n; i++)
+    {
+        int x; cin >> x;
+        while(x%p==0)
+        {
+            x /= p;
+            nrP++;
+        }
+        while (x%q==0)
+        {
+            x /= q;
+            nrQ++;
+        }
+    }
+    cout << (nrP < nrQ? nrP:nrQ);
+    return 0;
 }
