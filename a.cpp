cc2ac73d8b3732b84fb452a7758463469b36bcd4
David Pop <33874867+david7295@users.noreply.github.com>
2019-10-24 16:36:26 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,37 +1,25 @@
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-ifstream f("limite.in");
-ofstream g("limite.out");
+
 int main()
+
 {
-    double x,n;
-    f>>n;
-    for(int i=1;i<=n;i++)
-    {
-    f>>x;
-    if(x>=-128&&x<=127)
-    g<<"char"<<endl;
-    else
-    if(x>=0&&x<=256)
-    g<<"unsigned char"<<endl;
-    else
-    if(x>=-32768&&x<=32767)
-    g<<"short"<<endl;
-    else
-    if(x>=0&&x<=65535)
-    g<<"unsigned short"<<endl;
-    else
-    if(x>=-2147483648&&x<=2147483647)
-    g<<"int"<<endl;
-    else
-    if(x>=0&&x<=4294967295)
-    g<<"unsigned int"<<endl;
-    else
-    if(x>=-9223372036854775808&&x<=9223372036854775807)
-    g<<"long long"<<endl;
-    else
-    if(x>=0&&x<=18446744073709551615)
-    g<<"unsigned long long"<<endl;
-    }
-    return 0;
+
+int F, B, n, F1, B1;
+
+cin >> F >> B >> n;
+
+F1=3*n;
+
+B1=2*n;
+
+F=F1*F;
+
+B=B1*B;
+
+cout << F+B;
+
+return 0;
+
 }
