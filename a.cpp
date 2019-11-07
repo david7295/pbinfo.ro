79516f5d536da043c4d95e8a920e98a1beda5941
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-07 19:25:10 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,27 +1,50 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-using namespace std;
+using namespace std;
 
-int main()
+int n;
+
+unsigned long long Count(unsigned long long a);
+
+int main()
 {
-    int n; cin >> n;
-    for (int i=0; i<n; i++)
-    {
-        // Formula sirului e i*(2^i)+1
 
-        // Calculeaza 2^i
-        int p2 = 1;
-        for (int j=0; j<i; j++)
-            p2 *= 2;
-        int nr = i*p2+1;
+    cin >> n;
 
-        int nrDiv = 2; // 1 si numarul insusi
-        for (int j=2; j<=nr/2; j++)
-            if (nr%j==0)
-                nrDiv++;
-        if (nr == 1)
-            nrDiv = 1;
-        cout << nr << " - " << nrDiv << "\n";
-    }
-    return 0;
+    for(unsigned long long i=0;i<n;i++)
+    {
+        unsigned long long nr=i*(1ULL<<i)+1;
+            cout << Count(nr) << " ";
+    }
+
+    return 0;
+}
+
+unsigned long long Count(unsigned long long a)
+{
+   unsigned long long count = 1, k = 0, i;
+   if (a == 1 || a == 2)
+      return a;
+   while ((a & 1) == 0)
+   {
+      k++;
+      a >>= 1;
+   }
+   if (a == 1)
+      return k + 1;
+   else
+      count = k + 1;
+   for(i = 3; i*i <= a; i += 2)
+   {
+      k = 0;
+      while(a % i == 0)
+      {
+         k++;
+         a /= i;
+      }
+      count *= (k + 1);
+   }
+   if (a > 1)
+      count <<= 1;
+   return count;
 }
