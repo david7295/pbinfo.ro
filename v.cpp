304976091884e819f9f0df49b13a35148b33b36e
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-21 18:41:02 +0200

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -1,20 +1,51 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+
+#include <fstream>
+
 using namespace std;
+
+ifstream fin("easyquery.in");
+
+ofstream fout("easyquery.out");
+
+long long n,i,j,x,y,p,op,v[100002],A[100003],B[100003],T;
+
 int main()
-{
- ifstream in("prize.in");
-ofstream out("prize.out");
-int n;
-int tmp;
-in>>n;
-int s=0;
-for(int i=0;i<n;i++)
+
 {
 
-in>>tmp;
-s=s+(tmp-1);
-}
-s=s+1;
-out<<s;
+  fin >> n;
+
+  for (i=1; i<=n; ++i) fin >> v[i];
+
+  fin >> T;
+
+  for (i=1; i<=T; ++i)
+
+  {
+
+      fin >> op >> x >> y >> p;
+
+      if (op==1) { B[x]+=p; B[y+1]-=p; }
+
+      else { B[x]+=-p; B[y+1]-=-p; }
+
+  }
+
+  A[1]=B[1];
+
+   for (i=2; i<=n; ++i)
+
+       A[i]=A[i-1]+B[i];
+
+   for (i=1; i<=n; ++i) v[i]+=A[i];
+
+   for (i=1; i<=n; ++i)
+
+   {
+
+       fout << v[i] << " ";
+
+   }
+
 }
