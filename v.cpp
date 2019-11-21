434b66f3723e38232adb9489e5e31e9860e783b2
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-21 17:58:20 +0200

    Update v.cpp

--- v.cpp
+++ v.cpp
@@ -2,23 +2,44 @@
 
 using namespace std;
 
-int n,a,b,p;
+int n,k,a[1001],i,minim,maxim,j,S;
 
 int main()
 {
 
     cin >> n;
 
-    for(int i=1;i<=n;i++)
+    for(i=1;i<=n;i++)
+     cin >> a[i];
+
+    cin >>k;
+
+    minim=a[1];
+    maxim=a[1];
+
+    for(i=1;i<=n;i++)
     {
-        cin >> a >> b;
-        p=1;
-        while(p*2<=max(a,b))
-            p*=2;
-        if(p>=min(a,b)) cout << p << endl;
-        else cout << 0 << endl;
+        if(a[i]<minim) minim=a[i];
+        if(a[i]>maxim) maxim=a[i];
     }
 
-    return 0;
+   while(j<=k)
+    for(minim=minim;minim<=maxim;minim++)
+     for(i=1;i<=n;i++)
+      if(minim==a[i])
+             {
+                  j++;
+                  if(j<=k)
+                   a[i]=-a[i];
 
+             }
+
+
+
+    for(i=1;i<=n;i++)
+     S+=a[i];
+
+    cout << S;
+
+    return 0;
 }
