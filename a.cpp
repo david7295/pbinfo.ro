3debbf6018c10ee7543df122b9d1a0d8d2332fd7
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-26 16:23:41 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,19 +1,37 @@
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
-ifstream f("pc.in");
-ofstream g("pc.out");
-int T,x;
- 
+long long C, P, ga, va;
+int cer, d, nrdi, nrdp;
 int main()
 {
-    f>>T;
-    while(T--)
+    ifstream f("ograda.in");
+    ofstream g("ograda.out");
+    f >> cer;
+    f >> C >> P;
+    if(cer == 1)
     {
-        f>>x;
-        if(x%2)
-            g<<1<<' ';
-        else
-            g<<0<<' ';
+        va = (P - 2 * C) / 2;
+        ga = C - va;
+        g << ga << " " << va;
+    }
+    if(cer == 2)
+    {
+        for(d = 1; d * d < C; d ++)
+         if(C % d == 0)
+         {
+             if(d % 2 == 1) nrdi = nrdi + 1;
+             if((C / d) % 2 == 1) nrdi = nrdi + 1;
+         }
+         if(d * d == C && d % 2 == 1) nrdi = nrdi + 1;
+
+         for(d = 1; d * d < P; d ++)
+         if(P % d == 0)
+         {
+             if(d % 2 == 0) nrdp = nrdp + 1;
+             if((P / d) % 2 == 0) nrdp = nrdp + 1;
+         }
+         if(d * d == P && d % 2 == 0) nrdp = nrdp + 1;
+         g << nrdi << " " << nrdp;
     }
     return 0;
 }
