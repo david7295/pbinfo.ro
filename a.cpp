eb0e8637d0166f0c67a889595888caa72457c050
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-27 20:42:42 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,23 +1,51 @@
 #include <fstream>
 using namespace std;
-short v[200];
+ifstream fin("roata.in");
+ofstream fout("roata.out");
+
 int main()
 {
-int n,a=0,b=0,c=0,d=0,g,i;
-cin>>n;
-for(i=0;i<n;i++)
-{cin>>g;
-v[g-1]++;}
-for(i=0;i<200;i++)
-{if(v[i]==0) continue; //sare direct la următorul i în for dacă am dat de un 0; ne interesează doar nenulele
-c++;
-d=d+v[i]; //adunăm numerele pozitive
-if(c==3)
-{c=0;
-if(d%2==1)a=a+1;else b=b+1; //numărăm direct
-d=0;}
-}
-a=a%10;b=b%10; //a sau b ar putea să fi depășit 9
-cout<<10*a+b;
+
+int n,p,i,k,pmax,pmin,b[361],a[100001];
+long long s=0;
+
+fin>>n>>p;
+for(i=1;i<=n;i++)
+    b[i]=i;
+for(i=1;i<=p;i++)
+        fin>>a[i],s=s+a[i];
+fout<<s<<"\n";
+
+if (n<p)
+    for(k=n+1;k<=p;k++)
+        {
+        pmin=1;
+        for(i=2;i<=n;i++)
+            if (a[i]<a[pmin]) pmin=i;
+        fout<<b[pmin]<<" ";
+        if (a[pmin]>10000000)
+            for(i=1;i<=n;i++)
+                    a[i]-=10000000;
+
+        a[pmin]+=a[k];
+        b[pmin]=k;
+        }
+    else
+        n=p;
+pmax=1;
+for(i=2;i<=n;i++)
+    if (a[i]>=a[pmax]) pmax=i;
+
+for(k=1;k<=n;k++)
+        {
+        pmin=1;
+        for(i=2;i<=n;i++)
+            if (a[i]<a[pmin]) pmin=i;
+        fout<<b[pmin]<<" ";
+
+        a[pmin]=a[pmin]+100001;
+        }
+fout<<"\n";
+fout<<pmax<<"\n";
 return 0;
 }
