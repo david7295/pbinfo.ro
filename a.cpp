f88e8d48209e9f54dfd56a76dc1224ee1a5e019e
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-27 22:05:59 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,43 +1,66 @@
 #include <fstream>
-
+#include <iostream>
+#include <cstring>
+#include <cctype>
 using namespace std;
-ifstream f("cautanrinmatrice.in");
-ofstream g("cautanrinmatrice.out");
-int i , j  , x , a[1000008] , m , n , p , li , co , d , poz , mij ;
 
-int caut( int st , int dr )
+ifstream fin("cuvinte3.in");
+ofstream fout("cuvinte3.out");
+
+int n;
+char lista[505][256];
+
+void to_lower(char * p)
 {
-    if ( st > dr ) return 0 ;
-    else {
-           mij = ( st + dr ) / 2 ;
-           if ( a [ mij ] == x ) return mij ;
-           else if ( a [ mij ] < x ) return caut ( mij + 1 , dr ) ;
-                                else return caut ( st , mij - 1 ) ;
+for(int i = 0 ; p[i] ; i++)
+p[i] = tolower(p[i]);
+}
 
-         }
+bool OK(char * p)
+{
+char t[256];
+strcpy(t , p);
+to_lower(t);
+if( strstr(t , "ate") )
+return true;
+return false;
+}
+
+bool egale(char * s , char * t)
+{
+char ss[256], tt[256];
+strcpy(ss , s);
+strcpy(tt , t);
+to_lower(ss); to_lower(tt);
+if(strcmp(ss , tt) == 0)
+return true;
+return false;
 }
 
 int main()
 {
-    f >> n >> m ;
-    d = n * m ;
-    for ( i = 1 ; i <= n ; i++ )
-       if ( i % 2 == 1 ) for ( j = 1 ; j <= m ; j++ ) f >> a [ ( i - 1 ) * m + j ] ;
-                    else for ( j = 1 ; j <= m ; j++ ) f >> a [ i * m - j + 1 ] ;
-    f >> p ;
-    for ( i = 1 ; i <= p ; i++ )
-    {
-        f >> x;
-        poz = caut(1,d) ;
-        if ( poz==0 ) g << 0 << "\n" ;
-              else
-              {
-                  if( poz%m==0 ){ li = poz / m ; co = m ; }
-                           else { li = poz /m + 1 ; co = poz % m ; }
-                  if( li%2==1 ) g << li << " " << co << "\n" ;
-                           else g << li << " " << m - co + 1 << "\n" ;
-              }
-    }
-
-    return 0 ;
+char linie[256], sep[]=".,;:-?! ";
+while( fin.getline(linie , 256) )
+{
+char * p = strtok(linie , sep);
+while(p)
+{
+if(OK(p))
+{
+bool gasit = false;
+for(int i = 1 ; i <= n && ! gasit ; i++)
+if( egale(p , lista[i]) )
+gasit = true;
+if(! gasit)
+{
+n ++;
+strcpy(lista[n] , p);
+}
+}
+p = strtok( NULL , sep);
+}
+}
+for(int i = 1 ; i <= n ; i ++)
+fout << lista[i] << "\n";
+return 0;
 }
