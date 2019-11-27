6894b7a7cfae6e05dd1bbae55cfc32c685f305a6
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-27 22:03:38 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -2,21 +2,37 @@
 #include <cstring>
 using namespace std;
 
-int apcar(char * s , char c)
-{
-int cnt = 0;
-for(int i = 0 ; s[i] ; i++)
-if(s[i] == c)
-cnt ++;
-return cnt;
-}
+char s[2560], v[100][16] , * t;
+int n;
 
-int main(){
-char s[256], v[]="aeiouAEIOU";
-cin.getline(s, 256);
-int cnt = 0;
-for(int i = 0 ; v[i] ; i ++)
-cnt += apcar(s, v[i]);
-cout << cnt;
+
+int main()
+{
+cin.getline(s , 2560);
+t = strtok(s , " ");
+while(t)
+{
+int poz = -1;
+for(int i = 0 ; i < n && poz == -1 ; i ++)
+if(strcmp(t , v[i]) == 0)
+poz = i;
+if(poz == -1)
+{
+strcpy(v[n] , t);
+n ++;
+}
+t = strtok(NULL , " ");
+}
+for(int i = 0 ; i < n ; i ++)
+for(int j = i + 1 ; j < n ; j ++)
+if(strcmp(v[i] , v[j]) > 0)
+{
+strcpy(s , v[i]);
+strcpy(v[i] , v[j]);
+strcpy(v[j] , s);
+}
+for(int i = 0 ; i < n ; i ++)
+cout << v[i] << "\n";
+
 return 0;
 }
