2870d9f8ecfe7d4421782f5e14fdb55d46432f1c
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-27 22:19:30 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,53 +1,33 @@
 #include <fstream>
-#include <queue>
-#define INF 1000000000
 using namespace std;
 
-int A[1001][1001],B[1001][1001],n,m,im,jm,ic,jc;
-const int di[]={1,-1,0,0},dj[]={0,0,1,-1};// Deplasarea spre magazin
-queue<pair<int,int> >Q;// Initializarea cozii
-
-void citire()
-{
-    ifstream  f("ubuph.in");
-    f>>n>>m;
-    for(int i=1;i<=n;++i)
-        for(int j=1;j<=m;++j)
-        {
-            f>>A[i][j];
-            B[i][j]=INF;
-        }
-    f>>im>>jm>>ic>>jc;
-    f.close();
-}
-
-bool verif(int i,int j)// Verifica daca pozitia curenta se afla in matrice
-{
-    return 1<=i&&i<=n&&1<=j&&j<=m;
-}
-
-void Lee()
-{   B[ic][jc]=A[ic][jc];
-    while(!Q.empty())
-    {
-        pair<int,int> P=Q.front();
-        for(int k=0;k<4;++k)
-        {
-            int i=P.first+di[k],j=P.second+dj[k];
-            if(verif(i,j)&&B[i][j]>B[P.first][P.second]+A[i][j])
-                B[i][j]=B[P.first][P.second]+A[i][j],Q.push(make_pair(i,j));
-        }
-        Q.pop();
-    }
-}
+ifstream fin("crescator1.in");
+ofstream fout("crescator1.out");
 
 int main()
 {
-    citire();
-    Q.push(make_pair(ic,jc));
-    Lee();
-    ofstream g("ubuph.out");
-    g<<B[im][jm];
-    g.close();
-    return 0;
+    int a[100000], n, x, cnt = 0, y = 0;
+
+    fin>>n;
+    for(int i = 0; i < n; ++i)
+        fin>>a[i];
+
+    fin>>x;
+    while(x > 9)
+    {
+        for(int i = 0; i < n; ++i)
+            if(a[i] == x) cnt++;
+
+        int s = 0;
+        while(x)
+            {
+                s += x % 10;
+                x /= 10;
+            }
+        x = s;
+    }
+    for(int i = 0; i < n; ++i)
+        if(a[i] == x) cnt++;
+
+    fout<<cnt;
 }
