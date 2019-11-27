184d5959cd543d954fc7809f0bc7a711ae2e76bd
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-27 21:23:21 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,24 +1,45 @@
-bool cifre(int numar)
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main()
+
 {
-        int c = numar % 10;
 
-        while (numar > 0)
-        {
-               if (numar % 10 == c)
-                        numar /= 10;
-                else
-                        break;
-        }
+long long x,y,n,m, mx = -3;
 
-      return (numar == 0);
-}
+   cin >> x >> y >> n >> m;
 
+    long long colt1, colt2, colt3, colt4;
 
-void identice(int a, int b)
-{
-        for (int i = a; i <= b; i++)
-        {
-                  if (cifre(i))
-                        cout << i << " ";
-        }
+   colt1 = x - 1 + y - 1 + 1;
+
+   if(colt1 > mx)
+
+       mx = colt1;
+
+   colt2 = x - 1 + m - y +1;
+
+   if(colt2 > mx)
+
+       mx = colt2;
+
+   colt3 = n - x + y - 1 +1;
+
+   if(colt3 > mx)
+
+       mx = colt3;
+
+   colt4 = n - x + m - y +1;
+
+   if(colt4 > mx)
+
+       mx = colt4;
+
+   cout << mx;
+
+   return 0;
+
 }
