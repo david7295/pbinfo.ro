a7d4630f86615650a3ee99769bc1ca05946ba8c4
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-27 22:23:50 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,26 +1,51 @@
-#include <fstream>
-using namespace std;
-ifstream cin("mincifre.in");
-ofstream cout("mincifre.out");
-char c;
-int x[10];
+#include <iostream>
 
-int main()
+#include <fstream>
+
+#include <cstring>
+
+using namespace std;
+
+char s[255], ch;
+
+short n, i, j, cod;
+
+int main()
+
 {
-    c=cin.get();
-    while(!cin.eof())
-    {
-        x[c-48]++;
-        c=cin.get();
-    }
-    int n=1;
-    while(x[n]==0)
-        n++;
-    cout<<n;
-    x[n]--;
-    for(int i=1;i<=x[0];i++)
-        cout<<0;
-    for(int i=n;i<10;i++)
-        for(int j=1;j<=x[i];j++)
-            cout<<i;
+
+  cin >> s;
+
+  n=strlen(s);
+
+  for (i=0; i<n-1; ++i)
+
+  {
+
+     if (s[i]==s[i+1] || (int)s[i]-(int)s[i+1]==32 || (int)s[i]-(int)s[i+1]==-32 )
+
+     {
+
+         while (s[i]==s[i+1] || (int)s[i]-(int)s[i+1]==32 || (int)s[i]-(int)s[i+1]==-32)
+
+         {
+
+             for (j=i; j<n-1; ++j) s[j]=s[j+1];
+
+             --n; s[n]=0;
+
+         }
+
+          for (j=i; j<n-1; ++j) s[j]=s[j+1];
+
+             --n; s[n]=0;
+
+          i=-1;
+
+     }
+
+  }
+
+  cout << s;
+
 }
