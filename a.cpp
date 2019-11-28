d05022af59b09545a8c04c03d761177d11549ca2
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 17:58:16 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,23 +1,31 @@
-#include <iostream>
-using namespace std;
-int n ,i, d, nr, maxx=-1, m, p;
-long long s;
-int main()
-{
-cin >> m;
-for(i=1;i<=m;i++)
-{
-    n=i;
-    nr=0;
-        for(d = 1 ; d * d < n ; d++)
-   if(n % d == 0)
-     nr++;
-             nr = nr * 2;
-             if(d * d == n)
-        nr = nr + 1;
+#include <iostream>
 
-if(nr>maxx) maxx = nr, p = i;
-}
-cout<<p;
-return 0;
+using namespace std;
+
+int main(){
+
+   long long s, n, i;
+
+   cin >> n;
+
+   s = 0;
+
+   if (n%2 == 0)
+
+       s += 2;
+
+   for (i = 3;i*i <= n;i += 2){
+
+       if (n%i == 0){
+
+           s += i;
+
+       }
+
+   }
+
+   cout << s;
+
+   return 0;
+
 }
