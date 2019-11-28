1a78e4aaebe72b71e532e14a5ade4974d3f42eb9
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 17:43:54 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,49 +1,29 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
-
-int main()
-{
-   long long int x, y, a, b, z, c, nrcifx, nrcify, nr1, nr2, inv1, inv2, k = 0;
-    do
-    {
-        cin >> x;
-        cin >> y;
-        nrcifx = 0;
-        nrcify = 0;
-        a = x;
-        while (a != 0)
-        {
-            nrcifx = nrcifx + 1;
-            a = a / 10;
-        }
-        b = y;
-        while (b != 0)
-        {
-            nrcify = nrcify + 1;
-            b = b / 10;
-        }
-        nr1 = x*pow(10, nrcify) + y;
-        nr2 = y*pow(10, nrcifx) + x;
-        z = nr1;
-        c = nr2;
-        inv1 = 0;
-        while (nr1)
-        {
-            inv1 = inv1 * 10 + nr1 % 10;
-            nr1 = nr1 / 10;
-        }
-        inv2 = 0;
-        while (nr2)
-        {
-            inv2 = inv2 * 10 + nr2 % 10;
-            nr2 = nr2 / 10;
-        }
-        if (z == inv1 || c == inv2)
-            k = k + 1;
-        if (x == 0 && y == 0)
-            k = k - 1;
-    } while (x != 0 && y != 0);
-    cout << k;
-    return 0;
+#include <fstream> 
+#include <cassert>
+using namespace std; 
+ifstream fin("ordsume.in");
+ofstream fout("ordsume.out"); 
+int n,m, a[101], s[10001];
+int main(){ 
+  fin >> n;
+  for(int i=1;i<=n;++i)
+    fin >> a[i];
+  m = 0; 
+  for(int i=1;i<n;++i)
+    for(int j=i+1 ; j<=n ; j++)
+      if(a[i]!=a[j]) { 
+        int gasit = 0; 
+        for(int k=1 ; k<=m && !gasit ; ++k) 
+          if(s[k] == a[i]+a[j]) gasit = 1;
+        if(!gasit) s[++m] = a[i]+a[j]; } 
+  for(int i=1;i<m;++i) 
+    for(int j=i+1 ; j<=m ; ++j)
+      if(s[i]>s[j]){
+        int aux = s[i]; 
+        s[i] = s[j];
+        s[j] = aux; } 
+  for(int i=1;i<=m;++i) 
+    fout << s[i] << " ";
+  return 0; 
 }
