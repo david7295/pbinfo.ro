ed2dc1c06871656c999815269dab3ef457fc1f0b
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 17:47:46 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,29 +1,91 @@
-#include <iostream>
-#include <fstream> 
-#include <cassert>
-using namespace std; 
-ifstream fin("ordsume.in");
-ofstream fout("ordsume.out"); 
-int n,m, a[101], s[10001];
-int main(){ 
-  fin >> n;
-  for(int i=1;i<=n;++i)
-    fin >> a[i];
-  m = 0; 
-  for(int i=1;i<n;++i)
-    for(int j=i+1 ; j<=n ; j++)
-      if(a[i]!=a[j]) { 
-        int gasit = 0; 
-        for(int k=1 ; k<=m && !gasit ; ++k) 
-          if(s[k] == a[i]+a[j]) gasit = 1;
-        if(!gasit) s[++m] = a[i]+a[j]; } 
-  for(int i=1;i<m;++i) 
-    for(int j=i+1 ; j<=m ; ++j)
-      if(s[i]>s[j]){
-        int aux = s[i]; 
-        s[i] = s[j];
-        s[j] = aux; } 
-  for(int i=1;i<=m;++i) 
-    fout << s[i] << " ";
-  return 0; 
+#include <fstream>
+
+
+using namespace std;
+
+
+ifstream fin("pinguini.in");
+
+
+ofstream fout("pinguini.out");
+
+
+int n, a[10001], i,lg,lgmax,nrp,nrg,c;
+
+
+int main()
+
+
+{
+
+
+   fin>>c>>n;
+
+
+   for(i=1;i<=n;i++)
+
+
+       {
+
+
+           fin>>a[i];
+
+
+           if(a[i]==2)nrp++;
+
+
+       }
+
+
+   if(a[1]==1)nrg=0;
+
+
+   else nrg=1;
+
+
+   for(i=2;i<=n;i++)
+
+
+           if(a[i]==2&&a[i-1]==1)nrg++;
+
+
+   lg=lgmax=0;
+
+
+   for(i=1;i<=n;i++)
+
+
+   if(a[i]==2)lg++;
+
+
+   else
+
+
+   {
+
+
+       if(lg>lgmax)lgmax=lg;
+
+
+       lg=0;
+
+
+   }
+
+
+   if(lg>lgmax)lgmax=lg;
+
+
+   if(c==1)fout<<nrp<<endl;
+
+
+   if(c==2)fout<<nrg<<endl;
+
+
+   if(c==3)fout<<lgmax<<endl;
+
+
+   return 0;
+
+
 }
