387699702558736b53b7b115721b881400af3f85
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 17:36:20 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,36 +1,80 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long divMaxImpar(long long num)
+ifstream fin("odaoni.in");
+
+ofstream fout("odaoni.out");
+
+ 
+
+int n, cf[10], d, aux, nrdiv=1;
+
+int multimecifre(int nr)
 
 {
 
-   while (num%2==0)
-
-       num/=2;
-
-   return num;
-
-}
-
-int  main()
-
-{
-
-   long long n, i, num;
-
-   cin >> n;
-
-   for (i=1; i<=n; ++i)
+   while(nr)
 
    {
 
-       cin >> num;
+       if(cf[nr%10]==0)
 
-       cout << divMaxImpar(num) << " ";
+           return 0;
+
+       nr/=10;
 
    }
+
+   return 1;
+
+}
+
+int main()
+
+{
+
+   fin>>n;
+
+   aux=n;
+
+   while(aux)
+
+   {
+
+       cf[aux%10]++;
+
+       aux/=10;
+
+   }
+
+   if(cf[1])
+
+       nrdiv++;
+
+    for(d=2; d*d<n;d++)
+
+       if(n%d==0)
+
+       {
+
+           if(multimecifre(d))
+
+               nrdiv++;
+
+           if(multimecifre(n/d))
+
+               nrdiv++;
+
+       }
+
+   if(d*d==n)
+
+       if(multimecifre(d))
+
+           nrdiv++;
+
+   fout<<nrdiv;
 
    return 0;
 
