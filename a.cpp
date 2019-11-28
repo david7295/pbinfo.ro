d2a74b10b041424510bc6b5a65121f2f30ed2016
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 19:37:38 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,49 +1,17 @@
-#include <iostream>
-
 #include <fstream>
-
 using namespace std;
 
-int a[26][26];
-
-int scol[26];
+ifstream fin("maxn.in");
+ofstream fout("maxn.out");
 
 int main()
-
 {
-
-   ifstream f("sumcolmax.in");
-
-   ofstream g("sumcolmax.out");
-
-   int n, m, i, j, max, ind;
-
-   f >> n >> m;
-
-   for (i=1; i<=n; ++i)
-
-   {
-
-       for (j=1; j<=m; ++j)
-
-       {
-
-           f >> a[i][j]; scol[j]+=a[i][j];
-
-       }
-
-   }
-
-   max=scol[1]; ind=1;
-
-   for (j=2; j<=m; ++j)
-
-        if (scol[j]>max) { max=scol[j]; ind=j; }
-
-   for (i=1; i<=n; ++i)
-
-       g << a[i][ind] << " ";
-
-   return 0;
-
+    int n, x, mx = 0;
+    fin >> n;
+    while (n--)
+    {
+        fin >> x;
+        if (x > mx) mx = x;
+        fout << mx << ' ';
+    }
 }
