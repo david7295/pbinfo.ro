833e6cc6246ddca2941ca41854716daa5be94d24
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 18:56:31 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,33 +1,13 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-
-{
-
-   int n, m, p;
-
-   cout<<"Introduceti numarul n : ";
-
-   cin>>n;
-
-   cout<<"Introduceti numarul p : ";
-
-   cin>>p;
-
-   m=n;
-
-   while(m<p)
-
-   {
-
-       cout<<m<<" ";
-
-       m=m*n;
-
-   }
-
-   return 0;
-
-}
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n, p,i;
+    long long res;
+    cin>>n>>p;
+    for(i=0;i<p;i++){
+        res = pow(n,i);
+        if(res<=p)cout<<res<<" ";
+        else break;
+    }
+    return 0;
