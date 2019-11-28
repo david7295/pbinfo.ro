668c4626701cf9dfbddbe7ff21a1cfa8961ebdae
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 17:50:25 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,91 +1,23 @@
-#include <fstream>
-
-
-using namespace std;
-
-
-ifstream fin("pinguini.in");
-
-
-ofstream fout("pinguini.out");
-
-
-int n, a[10001], i,lg,lgmax,nrp,nrg,c;
-
-
-int main()
-
-
+#include <iostream>
+using namespace std;
+int n ,i, d, nr, maxx=-1, m, p;
+long long s;
+int main()
 {
+cin >> m;
+for(i=1;i<=m;i++)
+{
+    n=i;
+    nr=0;
+        for(d = 1 ; d * d < n ; d++)
+   if(n % d == 0)
+     nr++;
+             nr = nr * 2;
+             if(d * d == n)
+        nr = nr + 1;
 
-
-   fin>>c>>n;
-
-
-   for(i=1;i<=n;i++)
-
-
-       {
-
-
-           fin>>a[i];
-
-
-           if(a[i]==2)nrp++;
-
-
-       }
-
-
-   if(a[1]==1)nrg=0;
-
-
-   else nrg=1;
-
-
-   for(i=2;i<=n;i++)
-
-
-           if(a[i]==2&&a[i-1]==1)nrg++;
-
-
-   lg=lgmax=0;
-
-
-   for(i=1;i<=n;i++)
-
-
-   if(a[i]==2)lg++;
-
-
-   else
-
-
-   {
-
-
-       if(lg>lgmax)lgmax=lg;
-
-
-       lg=0;
-
-
-   }
-
-
-   if(lg>lgmax)lgmax=lg;
-
-
-   if(c==1)fout<<nrp<<endl;
-
-
-   if(c==2)fout<<nrg<<endl;
-
-
-   if(c==3)fout<<lgmax<<endl;
-
-
-   return 0;
-
-
+if(nr>maxx) maxx = nr, p = i;
+}
+cout<<p;
+return 0;
 }
