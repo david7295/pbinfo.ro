b01ca17cfc4d1a3009d878056d0dd029b961de0e
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 20:59:24 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,72 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
-int n,i,grup,nr,k,v[105],j;
-bool ok,ok1;
-struct elev
+#include <iostream>
+
+using namespace std;
+
+int n, k, i, m, cif, num;
+
+int main()
+
 {
-    char num[55];
-    char pren[55];
-    int el;
-}p[105],aux;
-int main()
-{
-    freopen("serbare1.in","r",stdin);
-    ofstream g("serbare1.out");
-   scanf("%d\n",&n);
-    for(i=1;i<=n;i++)
-    scanf("%s %s %d",&p[i].num,&p[i].pren,&p[i].el);
 
-    do
-    {
-        ok=true;
-        for(i=1;i<n;i++)
-        if(p[i].el<p[i+1].el)
-        {
-            aux=p[i];
-            p[i]=p[i+1];
-            p[i+1]=aux;
-            ok=false;
-        }
-    }while(!ok);
-  nr=1;
-  v[1]=1;
-    for(i=1;i<=n;i++)
-       if(p[i].el!=p[i+1].el)
-       {
-           do
-       {
-        ok=true;
-        for(j=i-v[nr]+1;j<i;j++)
-         {
-             ok1=true;k=0;
-         while(ok1)
-        {
+   cin >> n;
 
-            if(p[j].num[k]>p[j+1].num[k])
-            {
-            aux=p[j];
-            p[j]=p[j+1];
-            p[j+1]=aux;
-            ok=false;
-            ok1=false;
-            }
-            else if(p[j].num[k]==p[j+1].num[k]) k++;
-            else ok1=false;
-        }
-         }
-        }while(!ok);
+   for (i=1; i<=n; ++i)
 
+   {
 
-           nr++;v[nr]=1;
-        }
-       else v[nr]++;
-    g<<nr-1;
-    nr=0;
-    for(i=1;i<=n;i++)
-    {
-         if(p[i-1].el!=p[i].el) {nr++;g<<'\n'<<v[nr]<<' ';}
-         g<<p[i].num<<" "<<p[i].pren<<" ";
+       cin >> k;
 
-    }
-    return 0;
+       num=100; m=3;
+
+       while (m<k)
+
+       {
+
+           m+=3; ++num;
+
+       }
+
+       if (m-k==0) cif=num%10;
+
+       if (m-k==1) cif=(num/10)%10;
+
+       if (m-k==2) cif=num/100;
+
+       cout << cif << " ";
+
+   }
+
 }
