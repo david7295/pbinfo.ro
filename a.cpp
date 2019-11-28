a2fb5ff7a7d78220ed23c6fbfb364a0da93b7006
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-28 19:36:31 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,13 +1,49 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main(){
-    int n, p,i;
-    long long res;
-    cin>>n>>p;
-    for(i=0;i<p;i++){
-        res = pow(n,i);
-        if(res<=p)cout<<res<<" ";
-        else break;
-    }
-    return 0;
+#include <iostream>
+
+#include <fstream>
+
+using namespace std;
+
+int a[26][26];
+
+int scol[26];
+
+int main()
+
+{
+
+   ifstream f("sumcolmax.in");
+
+   ofstream g("sumcolmax.out");
+
+   int n, m, i, j, max, ind;
+
+   f >> n >> m;
+
+   for (i=1; i<=n; ++i)
+
+   {
+
+       for (j=1; j<=m; ++j)
+
+       {
+
+           f >> a[i][j]; scol[j]+=a[i][j];
+
+       }
+
+   }
+
+   max=scol[1]; ind=1;
+
+   for (j=2; j<=m; ++j)
+
+        if (scol[j]>max) { max=scol[j]; ind=j; }
+
+   for (i=1; i<=n; ++i)
+
+       g << a[i][ind] << " ";
+
+   return 0;
+
+}
