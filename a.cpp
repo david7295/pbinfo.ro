ba973e0616690b38f6442e06dbf72b92b4b869d8
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-29 21:16:49 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,65 +1,40 @@
-#include <fstream>
+#include <iostream>
 using namespace std;
-ifstream f("memory006.in");
-ofstream g("memory006.out");
-long long n,k,i,suma,nr,x,st,dr,p[60];
-long mij;
-char a[10001],t,j;
-
-char exp(long s,long d)
-{
-    if(s>d) return 0;
-    mij=(s+d)/2;
-    if(x==p[mij])
-        return mij;
-    if(x<p[mij])
-        return exp(s,mij-1);
-    else
-        return exp(mij+1,d);
-}
 
 int main()
 {
-    f>>n>>k;
-    p[1]=2;
-    for(i=2;i<=57;++i)
-        p[i]=p[i-1]*2;
-    nr=0;
-    suma=0;
-    st=0;
-    dr=-1 ;
-    for(i=0;i<n;++i)
-    {
-        f>>x;
-        t=exp(1,57);
-        if(t==0)
-        {
-           suma=0;
-           st=0;
-           dr=-1;
-        }
-        else
-        {
-           dr=(dr+1)%10001;
-           a[dr]=t ;
-           suma=suma+t;
-           if(suma>=k)
-           {
-              if(suma==k)
-                ++nr;
-              else
-              {
-                while(suma>k)
-              {
-                  suma=suma-a[st];
-                  st=(st+1)%10001;
-              }
-              if(suma==k)
-                ++nr;
-              }
-           }
-        }
-    }
-    g<<nr;
-    return 0;
+int a,b,c,n,i,k,s=0,nr=0,y;
+int p;
+cin>>n>>k>>p;
+a=1; b=2;  y=p%60;
+if(a==k)nr++;
+if(b==k)nr++;
+s=2;
+for(i=3;i<=n;i++)
+{
+c=(a+b)%10;  
+a=b; b=c;
+if((c==2)||(c==3)||(c==5)||(c==7))s=s+c;
+if(c==k)nr++;
+}
+a=1; b=2;
+if(y==1)
+c=a;
+else
+if(y==2)
+c=b;
+else
+{
+if(y==0)
+y=60;
+for(i=3;i<=y;i++)
+{
+c=(a+b)%10;  
+a=b; b=c;
+}
+}
+cout<<s;
+cout<<endl<<nr;
+cout<<endl<<c;
+return 0;
 }
