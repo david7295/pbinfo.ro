e881ee4f13679c26c9a4d461f0c20c111fc015e5
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-29 07:38:31 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int n,m,v[100][100],C=0;
-    cin>>n>>m;
-     for(int i=1;i<=n;i++)
-     for(int j=1;j<=m;j++)
-     {
-       cout<<"v["<<i<<"]["<<j<<"]=";
-       cin>>v[i][j];
-     }
-   for(int i=1;i<n;i++)
-     for(int j=i+1;j<=n;j++)
-     {    
-         bool ok=true;
-         int k=1;
-         while(k<=m && ok)
-         {
-             if(v[i][k]!=v[j][k] )
-                ok=false;
-             else
-                k++;
-         }
-         if(ok)
-            C++;
-     }
-     cout<<C;
-     return 0;
+
+void dinte(int n){
+  if(n == 1)
+    cout << 1 <<" ";
+  else if (n > 1) {
+    dinte(n - 1);
+    cout << n << " ";
+    dinte(n - 1);
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  dinte(n);
+  return 0;
 }
