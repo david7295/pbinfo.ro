830b06bb3c9d2d5dc8956a5a2bbc7ec3c6b1ea24
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-29 21:17:39 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,40 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main()
+int ct, k, p, n, v[105], i, s;
+int main ()
 {
-int a,b,c,n,i,k,s=0,nr=0,y;
-int p;
-cin>>n>>k>>p;
-a=1; b=2;  y=p%60;
-if(a==k)nr++;
-if(b==k)nr++;
-s=2;
-for(i=3;i<=n;i++)
-{
-c=(a+b)%10;  
-a=b; b=c;
-if((c==2)||(c==3)||(c==5)||(c==7))s=s+c;
-if(c==k)nr++;
-}
-a=1; b=2;
-if(y==1)
-c=a;
-else
-if(y==2)
-c=b;
-else
-{
-if(y==0)
-y=60;
-for(i=3;i<=y;i++)
-{
-c=(a+b)%10;  
-a=b; b=c;
-}
-}
-cout<<s;
-cout<<endl<<nr;
-cout<<endl<<c;
-return 0;
+    cin >> n >> k >> p;
+    v[1] = 1;
+    v[2] = 2;
+    for (i = 3; i<= 100; i ++)
+        v[i] = (v[i-1] + v[i-2]) % 10;
+    s = 0;
+    for (i = 1; i <= n; i ++)
+    {
+        if (v[i] == 2 || v[i] == 3 || v[i] == 5 || v[i] == 7)
+            s = s + v[i];
+        if (v[i] == k) ct ++;
+    }
+    p = p % 60;
+    cout << s << "\n";
+    cout << ct << "\n";
+    cout << v[p];
+    return 0;
 }
