9799a996e39b731819d4147ed12e78befc04430b
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-29 20:42:25 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,42 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
-int i, j, q, n, m, lu, k, k1, k2, k3;
-char s[15000], c[50], *p, sep[]=" ";
-string v1[105], v2[105], v3[105];
-int main()
-{
-    ifstream f("s_p_c.in");
-    ofstream g("s_p_c.out");
-    f.getline(s, sizeof(s));
-    p = strtok(s, sep);
-    while(p)
-    {
-        strcpy(c, p);
-        lu = strlen(c);
-        if(c[lu - 1] == 'S')
-        {
-            c[lu - 2] = NULL;
-            v1[++ k1] = c;
-        }
-        if(c[lu - 1] == 'P')
-        {
-            c[lu - 2] = NULL;
-            v2[++ k2] = c;
-        }
-        if(c[lu - 1] == 'C')
-        {
-            c[lu - 2] = NULL;
-            v3[++ k3] = c;
-        }
-            p = strtok(NULL, sep);
-    }
-    sort(v1 + 1, v1 + k1 + 1);
-    sort(v2 + 1, v2 + k2 + 1);
-    sort(v3 + 1, v3 + k3 + 1);
+#include <iostream>
 
-    for(i = 1; i <= k1; i ++)
-      for(j = 1; j <= k2; j ++)
-        for(q = 1; q <= k3; q ++)
-         g << v1[i] << " " << v2[j] << " " << v3[q] << "\n";
-    return 0;
+using namespace std;
+
+unsigned int nr_perechi(unsigned int n)
+
+{
+
+   unsigned int exp, i, np=1, a=n;
+
+   for (i=2; i*i<=a; ++i)
+
+   {
+
+       exp=0;
+
+      while (a%i==0)
+
+      {
+
+          ++exp;  a/=i;
+
+      }
+
+      if (exp!=0) np=np*(2*exp+1);
+
+   }
+
+   if (a!=1) np*=3;
+
+   return np;
+
+}
+
+int main()
+
+{
+
+   unsigned int n, x, i;
+
+   cin >> n;
+
+   for (i=1; i<=n; ++i)
+
+   {
+
+       cin >> x;
+
+       cout << nr_perechi(x) << " " ;
+
+   }
+
+   return 0;
+
 }
