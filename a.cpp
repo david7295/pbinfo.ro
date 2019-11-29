8a5117506a876fa96f39344b096107eed9344283
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-29 15:50:33 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
+
 using namespace std;
-int main() { 
-  long long n,p,p2=1; 
- cin>>n>>p;
- while(p2<=p) {
-     cout<<p2<<" "; 
-     p2=p2*n; } 
- return 0;
+
+int main()
+
+{
+
+   int n,i,num,urm, prima, ultima;
+
+   cin >> n;
+
+   cin >> num;
+
+   ultima=num%10;
+
+   for (i=2; i<=n; ++i)
+
+   {
+
+       cin >> urm;
+
+       prima=urm;
+
+       while (prima>9)
+
+       {
+
+           prima=prima/10;
+
+       }
+
+       if (ultima==prima)
+
+           cout << num << " " << urm << endl;
+
+       num=urm;
+
+       ultima=num%10;
+
+   }
+
 }
