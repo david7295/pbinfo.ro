90cb05cb85c9f04ca2de14a212ce079462bfc960
David Pop <33874867+david7295@users.noreply.github.com>
2019-11-30 09:32:09 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,39 +1,27 @@
-#include <bits/stdc++.h>
+#include <cstdio>
 using namespace std;
-unsigned long long start, stop;
-unsigned long long a, b, i, nr, p = 1;
-bool prim(long long x)
-{
-    bool ok = true;
-    long long i;
-    if(x == 0 or x == 1) return false;
-    for(i = 2; i * i <= x; i ++)
-    if(x % i == 0)
-    {
-        ok = false;
-        break;
-    }
-    return ok;
-}
+
+int v[100001], N, X, Q, nr;
 
 int main()
 {
-    cin >> a >> b;
-    start = log2(a) + 1;
-    stop = log2(b);
-    for(i = 1; i <= start; i ++)
-        p = p * 2;
-    if(prim(start))
-    {
-        nr = nr + p - a;
-    }
-    for(i = start; i < stop; i ++)
-        if(prim(i + 1))
+    freopen("divizori1.in", "r", stdin);
+    freopen("divizori1.out", "w", stdout);
+
+    scanf("%d %d", &N, &Q);
+
+    for (int i = 1; i <= N; i++)
         {
-            nr = nr + p;
-            p = p * 2;
-        } else p = p * 2;
-    if(prim(stop + 1)) nr = nr + b - p + 1;
-    cout << nr << " ";
+            scanf("%d", &X);
+            for (int j = X; j <= 100000; j += X)
+                v[j]++;
+        }
+
+    for (int i = 1; i <= Q; i++)
+    {
+        scanf("%d", &X);
+        printf("%d\n", v[X]);
+
+    }
     return 0;
 }
