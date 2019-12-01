6a1cd7671251fbbf49959aeff69b83e054788296
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 23:41:48 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a[100];
-    int N; cin >> N; //numar participanti
-    for (int i=0; i<N; i++)
-    {
-        int Xi, Yi; //Xi = distanta, Yi = viteza
-        cin >> Xi >> Yi;
-        a[i] = Xi/Yi;
-    }
-    cout << "\n";
-    int Q; cin >> Q; //numar intrebari
-    cout << "\n";
-    for (int i=0; i<Q; i++)
-    {
-        int nr=0;
-        int Qi; cin >> Qi;
-        for (int j=0; j<N; j++)
-            if (a[j]==Qi) nr++;
-        cout << nr << "\n\n";
-    }
-    return 0;
+int main() { 
+  int x,y,n,i,maxi=0,xf,yf;
+  cin>>n; 
+  for(i=1;i<=n;i++) {
+    cin>>x>>y; 
+    int d=max(x,y)-min(x,y); 
+    if(d>maxi) { 
+      xf=x;
+      yf=y;
+      maxi=d; 
+    } 
+  } 
+  cout<<xf<<" "<<yf;
+  return 0;
 }
