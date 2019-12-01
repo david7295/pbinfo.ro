28905f14f19da03e0fd2cf785c6e7d11ab277ec3
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 09:41:57 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,89 +1,60 @@
 #include <iostream>
-
-#include <fstream>
-
-#include <cstring>
-
-#include <algorithm>
-
 using namespace std;
 
-struct Copil
-
-{
-
-   char Nume[21];
-
-   int Numar;
-
-};
-
-char voc[]="aeiouAEIOU";
-
-bool acompare(Copil lhs, Copil rhs) {
-
-   if(lhs.Numar == rhs.Numar){
-
-       if (strcmp(lhs.Nume,rhs.Nume)<0)
-
-      return lhs.Nume < rhs.Nume;
-
-       }
-
-   return lhs.Numar < rhs.Numar;
-
-    }
-
-Copil v[300001] ;
-
 int main()
-
 {
+    int n, k, x;
+ 
+    cin >> k >> n >> x;
+  
+    //a)
+    int p = 1;  // p = 2^(k-1)
+    while (k > 1)
+    {
+        p *= 2;
+        k--;
+    }   
+    cout << p << '\n';
 
-  int n;
+   //b)
+   int i = 1, p = 1, b2 = 0;  /*b2 = numarul n in baza 2; nu e nevoie neaparat de el; p va lua pe rand puterile lui 2; va fi folosit pentru a-l construi pe b2
+    i = pozitia in numar, incepand de la ultima cifra*/
+   while (n)
+   {
+      //nu e nevoie de aceste doua randuri; aici se formeaza numarul in baza 2
+      b2 += (n % 2) * p;
+      p *= 2;
 
-  cin >> n;
+      /*Daca in baza 2, cifra este 1, atunci in sirul initial cifra va fi i, unde i este pozitia in numar; Daca este 0, atunci si in numarul initial este 0*/
+      if (n % 2 == 0) cout << i << ' ';
+      else                 cout << 0;
+      i++;
+      n /= 2;
+   }
 
-  for(int i = 1; i <= n; i++)
-
-  {
-
-      cin >> v[i].Nume;
-
-      char aux[21]="";
-
-      strcpy(aux,v[i].Nume);
-
-      int maxim = 0;
-
-      int l = strlen(aux);
-
-      for(int j = 0; j < l - 1; j++)
-
-      {
-
-          if(strchr(voc,aux[j]) && strchr(voc,aux[j + 1]))
-
-              maxim += 1;
-
-          if(!(strchr(voc,aux[j])) && !(strchr(voc,aux[j + 1])))
-
-              maxim -= 1;
-
-      }
-
-      v[i].Numar = maxim;
-
-  }
-
-  sort(v + 1, v + 1 + n,acompare);
-
-  for(int i = 1; i <= n; i++)
-
-  {
-
-      cout << v[i].Nume << "\n";
-
-  }
-
+   //c)
+   /*Vom face acelasi lucru ca la b, numai ca in loc sa afisam cifrele, le vom pune intr-o variabila*/
+ 
+   n = 1;
+   int last = 0, nr = 0;  //nr va lua pe rand valoarea fiecarui termen din sir
+   while (nr <= x)
+   {
+      int aux = n, p = 1; //p = puterile lui 10
+      i = 1;
+      nr = 0;
+      //generarea termenului de pe pozitia n/aux
+      while (aux)
+      {
+           if (aux % 2 == 1)
+               nr = nr + p * i;
+ 
+           p *= 10;
+           i++;
+           aux /= 2;
+      }
+      last = nr;
+      n++;
+   }
+   cout << last;
+return 0;
 }
