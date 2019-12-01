bc8c3564fbfc08c4d3723d29a4bc2b19a7625b9d
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 21:41:52 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,21 +1,43 @@
-#include <cstdio>
-using namespace std;
-long long n,i,j,x,y,p,q,r;
-int main() {
-  freopen("jumatate1.in","r",stdin);
-  freopen("jumatate1.out","w",stdout);
-  scanf("%lld\n",&n);
-  p=q=1; 
-  while (n/q>0) { 
-    p*=10;
-    q*=100; }
-  x=n/p;
-  y=n%p;
-  r=x%y;
-  while(r) {
-    x=y;
-    y=r;
-    r=x%y; }
-  printf("%lld\n",y);
-  return 0;
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("pitic.in");
+ofstream fout("pitic.out");
+
+int prime(int a);
+int sumdivprim(int n);
+int m;
+
+int main()
+{
+
+    fin >> m;
+
+    for(int i=1;i<=m;i++)
+        fout << sumdivprim(i) << " ";
+
+    return 0;
+
+}
+int prime(int a)
+{
+   int i;
+   if(a==2)
+      return 1;
+   if(a==0 or a==1 or a%2==0)
+      return 0;
+   for(i=3;i*i<=a and a%i;i+=2)
+      ;
+   return i*i>a;
+}
+int sumdivprim(int n)
+{
+    if(prime(n))
+        return n;
+    int s=0;
+    for(int i=2;i<=n/2;i++)
+        if(prime(i) and n%i==0)
+            s+=i;
+    return s;
 }
