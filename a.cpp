46dec4e3e25fef290f8002cc39d3a6f81d9a8e66
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 23:14:38 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,17 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#include <fstream>
+
 using namespace std;
 
-int main(){  
-  int a1, r, n,x,y;   
-  cin>>a1>>r>>n;  
-  x=a1;   
-  for(int i=1;i<=n;i++){   
-    y=x;     
-    for(int j=1;j<=n;j++){ 
-      cout<<y<<" ";      
-      y=y+n*r;   
-    } 
-    cout<<'\n'; 
-    x=x+r;  }
-    return 0;
+int main()
+
+{
+
+   ifstream f("conversie_b_10.in");
+
+   ofstream g("conversie_b_10.out");
+
+   int n, b, zecimal=0, z=1;
+
+   f >> n >> b;
+
+   while (n)
+
+   {
+
+       zecimal= zecimal+n%10*z;
+
+        n/=10; z=z*b;
+
+   }
+
+   g << zecimal;
+
+   return 0;
+
 }
