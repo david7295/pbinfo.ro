c0dfc5c9580ae7aea6ef02b4d411eeb00d6b9abb
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-02 00:50:43 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,35 +1,28 @@
-#include <fstream>
-
+#include <iostream> 
 using namespace std;
-
-ifstream f("daruri.in");
-
-ofstream g("daruri.out");
-
-int main()
-
-{   int a,b;
-
-   f>>a>>b;
-
-   int aux1=a,aux2=b;
-
-   while(b)
-
-   {   int r=a%b;
-
-       a=b;
-
-       b=r;
-
-   }
-
-   if(a==1)
-
-       g<<0<<'\n'<<0<<' '<<0;
-
-   else
-
-       g<<a<<'\n'<<aux1/a<<' '<<aux2/a;
-
+int palindrom(long int x) {
+  long int z=0,y;
+  y=x; 
+  while(x!=0){
+    z=z*10+x%10;
+    x=x/10; }
+  if(z==y) return 1; 
+  else return 0; }
+int prim(int n) { 
+  if(n==0 || n==1) return 0;
+  else if(n%2==0&&n!=2) return 0;
+  else for(int d=3;d*d<=n;d=d+2) 
+    if(n%d==0) return 0; 
+  return 1; }
+int norocos(int x) {
+  if(x%2) return 1;
+  else return 0; } 
+int main() { 
+  int n,x,nr=0; 
+  cin>>n; 
+  for(int i=1;i<=n;i++) {
+    cin>>x; 
+    if(palindrom(x) && prim(x) && norocos(x)) nr++; }
+  cout<<nr; 
+  return 0;
 }
