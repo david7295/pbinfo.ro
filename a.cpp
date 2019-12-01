aacb9adc22a70e4a023e060741736ffd15ad18dd
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-02 00:52:12 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,12 +1,53 @@
-#include <iostream>
-using namespace std;
-int n;
-int main()
+#include <iostream>
+
+using namespace std;
+
+int main()
+
 {
-    cin >> n;
-    if(n == 1) {cout << 0; return 0;}
-    if(n % 3 == 0) cout << (n/3) * 2;
-    if(n % 3 == 1) cout << (n/3) * 2;
-    if(n % 3 == 2) cout << (n/3) * 2 + 1;
-    return 0;
+
+unsigned a, b, nrSchimb=0, i, j, sus, jos, dreapta, stanga, colt;
+
+cin>>a>>b;
+
+sus=1; dreapta=1; jos=0; stanga=0;
+
+a++; b++;
+
+i=b; j=1;
+
+colt=0;
+
+while(!colt){
+
+ if(sus) i--;
+
+ if(dreapta) j++;
+
+ if(jos) i++;
+
+ if(stanga) j--;
+
+ if((i==1 && j==1) || (i==1 && j==a) || (i==b && j==1) || (i==b && j==a))
+
+  colt=1;
+
+ else{
+
+  if(i==1){jos=1; sus=0; nrSchimb++;}
+
+  if(i==b){jos=0; sus=1; nrSchimb++;}
+
+  if(j==1){dreapta=1; stanga=0; nrSchimb++;}
+
+  if(j==a){dreapta=0; stanga=1; nrSchimb++;}
+
+ }
+
+}
+
+cout<<nrSchimb;
+
+return 0;
+
 }
