d4ffadc9821433499f05fa9aadfcde5be80a15d3
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 22:43:43 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,99 +1,24 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
-
-int n, k, maxi, a[10001][103], l, x, y, smax = -1, elem = -1, s, cerinta;
-
-void proces(int nr)
-
-{
-
-   int aux = a[nr][a[nr][0]];
-
-   int j = a[nr][0]-1;
-
-   while (aux > a[nr][j] && j)
-
-       a[nr][j+1] = a[nr][j], j--;
-
-   a[nr][j+1] = aux;
-
-   while (a[nr][0] > k)
-
-       a[nr][0]--;
-
-}
-
-int main()
-
-{
-
-   cin >> k >> n >> x;
-
-   maxi = x;
-
-   for (int i = 2; i <= n; i++)
-
-   {
-
-       cin >> y;
-
-       maxi = max(maxi, y);
-
-       if (x == y)
-
-           l++;
-
-       else
-
-       {
-
-           a[x][0]++;
-
-           a[x][a[x][0]] = l;
-
-           proces(x);
-
-           l = 1;
-
-       }
-
-       x = y;
-
-   }
-
-   a[x][0]++;
-
-   a[x][a[x][0]] = l;
-
-   proces(x);
-
-   for (int i = 0; i <= maxi; i++)
-
-   {
-
-       s = 0;
-
-       for (int j = 1; j <= a[i][0]; j++)
-
-           s += a[i][j];
-
-       if (s >= smax)
-
-           smax = s, elem = i;
-
-   }
-
-   cin >> cerinta;
-
-   if (cerinta == 1)
-
-       cout << smax;
-
-   else
-
-       cout << elem;
-
-   return 0;
-
+int main(){
+  int n,i,j;
+  int a[30][30];
+  cout<<"Introduceti dimensiunea matricei: "; 
+  cin>>n;
+  for (i=0;i<n;i++){
+    for (j=0;j<n;j++){ 
+      if(i+j<n){ 
+        a[i][j]=i+j+1; }
+      else{ 
+        a[i][j]=2*n-1-(i+j); }
+    } 
+  }
+  cout<<"Matricea formata este: "<<endl;
+  for (i=0;i<n;i++){
+    for (j=0;j<n;j++){
+      cout<<a[i][j]<<" "; 
+    } 
+    cout<<endl; 
+  } 
+  return 0;
 }
