edc12b800ca902e38c01bbe417ce99e5dab34c85
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-02 00:57:49 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,27 +1,17 @@
-#include <cstdio>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
-int a, i, j, nr, p, w[105];
-char s[105];
+
+int n, i, nr;
+char s[75], *p;
 int main()
 {
-    gets(s + 1);
-    a = strlen(s + 1);
-    for(i = 1; i <= a; i ++)
+    cin.getline(s, sizeof(s));
+    p = strtok(s, " ");
+    while(p)
     {
-        if(s[i] == '.')
-            {
-            for(j = i + 1; j <= a; j ++)
-            if(s[j]!='0' && s[j]!='1' && s[j]!='2' && s[j]!='3' && s[j]!='4' && s[j]!='5' && s[j]!='6' && s[j]!='7' && s[j]!='8' && s[j]!='9') {p=j-1;break;}
-            if(p == 0) p = a;
-            for(j = i - nr; j <= p; j ++)
-            w[j] = true;
-            }
-            if(s[i] <= '9' && s[i] >= '0') nr ++;
-            else nr = 0;
-            p = 0;
+      if(strchr(p, 'a')) nr ++;
+      p = strtok(NULL, " ");
     }
-    for(i = 1; i <= a; i ++)
-        if(!w[i]) printf("%c", s[i]);
+    cout << nr;
     return 0;
 }
