77a3d2873e02e7148960d36c3ae239086a8965b5
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 23:47:24 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+
 using namespace std;
-int main() { 
-  int x,y,n,i,maxi=0,xf,yf;
-  cin>>n; 
-  for(i=1;i<=n;i++) {
-    cin>>x>>y; 
-    int d=max(x,y)-min(x,y); 
-    if(d>maxi) { 
-      xf=x;
-      yf=y;
-      maxi=d; 
-    } 
-  } 
-  cout<<xf<<" "<<yf;
-  return 0;
+
+int main()
+{
+   ifstream fin("conturi.in");
+   ofstream fout("coduri.out);
+
+   unsigned long long cod, sMax = 0, sCurent;
+   unsigned short N, X;
+   fin>>N>>X;
+   for(unsigned short i=1; i<=N; i++)
+   {
+      fin>>cod;
+      if(cod/1000000==X && cod/10000%10 == 1)
+      {
+         sCurent = 1000*cod/1000%10 + 100*cod/100%10 + 10*cod/10%10 + cod%10;
+        if(sCurent > sMax) sMax = sCurent;
+     }
+     fout<<sCurent;
 }
