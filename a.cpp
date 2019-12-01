d4a0828860d9cdc3fe1ee6924b393c6462766d0e
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-01 09:35:35 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,21 +1,97 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-using namespace std;
+#include <fstream>
 
-ifstream fin("cartonase.in");
-ofstream fout("cartonase.out");
+#include <algorithm>
 
-int n,m,S;
+using namespace std;
 
-int main()
+ifstream f("zece.in");
+
+ofstream g("zece.out");
+
+struct elev{
+
+   int probrez;
+
+   int poz;
+
+}v[1001];
+
+bool descr(elev a, elev b)
+
 {
 
-   fin >> n >> m;
+   if (a.probrez==b.probrez)
 
-   S=2*n*m-n-m;
+   {
 
-   fout << S;
+       return a.poz<b.poz;
 
-   return 0;
+   }
+
+   else return a.probrez>b.probrez;
+
+}
+
+int n,k,i, c;
+
+int main()
+
+{
+
+   f >> c >> n >> k;
+
+   for (i=1; i<=n; ++i)
+
+   {
+
+       f >> v[i].probrez;
+
+       v[i].poz=i;
+
+   }
+
+   sort(v+1,v+n+1,descr);
+
+   if (c==1)
+
+   {
+
+       for (i=1; i<=k; ++i)
+
+           g << v[i].probrez << " ";
+
+       i=k+1;
+
+       while (v[i].probrez==v[i-1].probrez)
+
+       {
+
+           g << v[i++].probrez << " ";
+
+       }
+
+   }
+
+   else
+
+   {
+
+       for (i=1; i<=k; ++i)
+
+           g << v[i].poz << " ";
+
+       i=k+1;
+
+       while (v[i].probrez==v[i-1].probrez)
+
+       {
+
+           g << v[i++].poz << " ";
+
+       }
+
+   }
 
 }
