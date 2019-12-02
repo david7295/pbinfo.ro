21e29328784c2e2e3226b9dc7af29a80534f2ae9
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-02 15:48:35 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,63 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int n,m,a[101][101];
 
-unsigned long Count(unsigned long a);
-
 int main()
 {
 
-   cin >> n >> m ;
+   cin >> n >> m;
 
    for(int i=1;i<=n;i++)
     for(int j=1;j<=m;j++)
      cin >> a[i][j];
 
    for(int i=1;i<=n;i++)
-    if(Count(a[i][1])<=2)
-     for(int j=1;j<=m/2;j++)
-      swap(a[i][j],a[i][m-j+1]);
+    for(int j=1;j<m;j++)
+     for(int k=j+1;k<=m;k++)
+      if(a[i][j]>a[i][k]) swap(a[i][j],a[i][k]);
 
    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=m;j++)
-         cout << a[i][j] << " ";
-        cout << endl;
-    }
-
-
+   {
+       for(int j=1;j<=m;j++)
+        cout << a[i][j] << " ";
+       cout << endl;
+   }
 
    return 0;
 
-}
-
-unsigned long Count(unsigned long a)
-{
-   unsigned long count = 1, k = 0, i;
-   if (a == 1 || a == 2)
-      return a;
-   while ((a & 1) == 0)
-   {
-      k++;
-      a >>= 1;
-   }
-   if (a == 1)
-      return k + 1;
-   else
-      count = k + 1;
-   for(i = 3; i*i <= a; i += 2)
-   {
-      k = 0;
-      while(a % i == 0)
-      {
-         k++;
-         a /= i;
-      }
-      count *= (k + 1);
-   }
-   if (a > 1)
-      count <<= 1;
-   return count;
 }
