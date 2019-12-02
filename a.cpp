06d6589c2988d7ad5b3e2746b017328a200f1fb1
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-02 15:51:03 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,30 +1,30 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int n,m,a[101][101];
-
-int main()
+#include <iostream>
+using namespace std;
+int main()
 {
-
-   cin >> n >> m;
-
-   for(int i=1;i<=n;i++)
-    for(int j=1;j<=m;j++)
-     cin >> a[i][j];
-
-   for(int i=1;i<=n;i++)
-    for(int j=1;j<m;j++)
-     for(int k=j+1;k<=m;k++)
-      if(a[i][j]>a[i][k]) swap(a[i][j],a[i][k]);
-
-   for(int i=1;i<=n;i++)
-   {
-       for(int j=1;j<=m;j++)
-        cout << a[i][j] << " ";
-       cout << endl;
-   }
-
-   return 0;
-
+    int n,m,v[100][100],C=0;
+    cin>>n>>m;
+     for(int i=1;i<=n;i++)
+     for(int j=1;j<=m;j++)
+     {
+       cout<<"v["<<i<<"]["<<j<<"]=";
+       cin>>v[i][j];
+     }
+   for(int i=1;i<n;i++)
+     for(int j=i+1;j<=n;j++)
+     {    
+         bool ok=true;
+         int k=1;
+         while(k<=m && ok)
+         {
+             if(v[i][k]!=v[j][k] )
+                ok=false;
+             else
+                k++;
+         }
+         if(ok)
+            C++;
+     }
+     cout<<C;
+     return 0;
 }
