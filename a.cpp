45e214391c7230da5537a4363d9d00ac3c8555ea
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-02 17:21:20 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,27 +1,23 @@
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-int v[2005][2005];
-int main(){ 
-  int n,m,i,j;  
-  cin>>n>>m; 
-  for(i=1;i<=n;++i){   
-    for(j=1;j<=m;++j){  
-      cin>>v[i][j];  }   
-  }    
-  for(i=1;i<=m;++i){  
-    int dp[2005]; 
-    for(j=1;j<=n;++j){    
-      dp[j]=v[j][i];        } 
-    sort(dp+1,dp+n+1);   
-    for(j=1;j<=n;++j){  
-      v[j][i]=dp[j];   
-    }   
-  }  
-  for(i=1;i<=n;++i){    
-    for(j=1;j<=m;++j){  
-      cout<<v[i][j]<<" ";   
-    }   
-    cout<<'\n';  
-  }   
-  return 0;
+
+int putere(int a, int b) {
+
+if (b == 1) return a;
+
+else return a * putere(a, b - 1);
+
+}
+
+int main() {
+
+int a, b;
+
+cout << "Introduceti a,b :";
+
+cin >> a >> b;
+
+cout << endl << putere(a, b);
+
 }
