1d2390539197205bb4866466d79a1e0680a99086
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-04 18:09:18 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
+
 using namespace std;
-int a [100][100],n,m,i,j,c,d,ok;
-int main ()
-{
-cin>>n>>m;
-for (i=1;i<=n;i++)
-for (j=1;j<=m;j++)
-cin>>a[i][j];
-for (i=1;i<=n;i++)
-for (j=1;j<=m;j++)
-if (i%2==0)
-{
-ok=1;
-if (a[i][j]==0||a[i][j]==1)
-ok=0;
-for (d=2;d<=a[i][j]/2;d++)
-if (a[i][j]%d==0)
-ok=0;
-if (ok)
-c++;
-}
-cout<<c;
+
+char ciur[1000000];
+
+int main(){
+
+   int n, m, nr = 0, i, j, k;
+
+   cin >> n >> m;
+
+   ciur[1] = ciur[0] = 1;
+
+   for (i = 2;i*i <= 1000000;i++)
+
+       if (ciur[i] == 0)
+
+        for (j = i*i;j <= 1000000;j += i)
+
+         ciur[j] = 1;
+
+   for (i = 1;i <= n;i++)
+
+       for (j = 1;j <= m;j++){
+
+        cin >> k;
+
+        if (j%2 == 1 && ciur[k] == 0)
+
+               nr++;
+
+    }
+
+   cout << nr;
+
+   return 0;
+
 }
