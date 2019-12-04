9113a91e60c37720e222ffb0ba7220f8c13671f0
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-04 18:20:14 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,41 +1,43 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main()
-
-{
-
-   long long a, b, d, schimb, m, c;
-
-   cin >> a >> b;
-
-   if (a==b) schimb=0;
-
-   else
-
-   {
-
-       if (a<b) { int t=a; a=b; b=t;}
-
-       d=a; m=b;
-
-      while (m) {
-
-       c = d % m;
-
-       d = m;
-
-       m = c;
-
-   }
-
-       schimb=b/d+a/d-2;
-
-   }
-
-   cout << schimb;
-
-   return 0;
-
+int main(){ 
+  int v[1001],cinci=0,doi=0,n,i,e,min,p;
+  cin>>n; 
+  for(i=1;i<=n;i++) { 
+    v[i]=i; 
+    e=v[i];
+    while(e%5==0||e%2==0) {
+      if(e%5==0) { 
+        cinci++;
+        e=e/5; 
+      }
+      if(e%2==0) {
+        doi++; 
+        e=e/2; 
+      } 
+    }
+  } 
+  if(doi<cinci) {
+    min=doi; } 
+  else { 
+    min=cinci; }
+  cinci=0; 
+  doi=0; 
+  for(i=1;i<=n;i++) {
+    while(v[i]%5==0&&cinci<min) {
+      v[i]=v[i]/5;
+      cinci++; } 
+    while(v[i]%2==0&&doi<min) {
+      v[i]=v[i]/2;
+      doi++; }
+    if(doi==min&&cinci==min) { 
+      break; } 
+  } 
+  p=1; 
+  for(i=1;i<=n;i++) 
+  {
+    p=(p*v[i])%10;
+  }
+  cout<<p;
+  return 0;
 }
