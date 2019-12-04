726e82d55bab5e3bb1b0a78d5c479c6c050929d3
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-04 18:24:47 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n, nr = 0, f = 5;
-    cin >> n;
-    while( f  <= n) {
-      int ff = f;
-      while( ff % 5 == 0) {
-        ff /= 5;
-        nr++;
-      }
-      f = f + 5;
-    }
-    cout << nr;
-    return 0;
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, nrd, d, i, x;
+int main()
+{
+    ifstream f("divk.in");
+    ofstream g("divk.out");
+    f >> n >> k;
+    for(i = 1; i <= n; i ++)
+    {
+        nrd = 0;
+        f >> x;
+        for(d = 1 ;d * d < x; d ++)
+            if(x % d == 0) nrd = nrd + 2;
+        if(d * d == x) nrd ++;
+        if(nrd >= k) g << x << " ";
+    }
+    return 0;
 }
