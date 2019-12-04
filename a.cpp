ebede24e3e0c5b8d91f55d3ded6fcfa7931693df
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-04 19:15:06 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,19 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
-int n, k, nrd, d, i, x;
-int main()
+#include <iostream>
+
+using namespace std;
+
+short v[100], m[100][100], n, i, j;
+
+int main()
+
 {
-    ifstream f("divk.in");
-    ofstream g("divk.out");
-    f >> n >> k;
-    for(i = 1; i <= n; i ++)
-    {
-        nrd = 0;
-        f >> x;
-        for(d = 1 ;d * d < x; d ++)
-            if(x % d == 0) nrd = nrd + 2;
-        if(d * d == x) nrd ++;
-        if(nrd >= k) g << x << " ";
-    }
-    return 0;
+
+   cin >> n;
+
+   for (i=0; i<n; ++i)
+
+       cin >> v[i];
+
+   for (j=0; j<n; ++j)
+
+   {
+
+       if (j%2==0)
+
+       {
+
+           for (i=0; i<n; ++i)
+
+               m[i][j]=v[n-1-i];
+
+       }
+
+       else
+
+       {
+
+           for (i=0; i<n; ++i)
+
+               m[i][j]=v[i];
+
+       }
+
+   }
+
+   for (i=0; i<n; ++i)
+
+   {
+
+       for (j=0; j<n; ++j)
+
+           cout << m[i][j] << " ";
+
+       cout << endl;
+
+   }
+
 }
