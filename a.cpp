d8f697b4f06690b0b42f1c56852d3630999e0f9e
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-04 17:55:31 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,42 +1,20 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int n,ok=0,i,j,a,b,c;
-    a=b=1;
-    cin>>n;
-    int arr[n+1][n+1];
-     for(i=1;i<=n;i++)
-     {
-        if(i%2!=0)
-       {
-         for(j=1;j<=n;j++)
-         {
-          if(i==1 && j<3)
-          arr[i][j]=1;
-          else
-          {
-          c=a+b;
-          arr[i][j]=c;
-          a=b;
-          b=c;
-          }
-         }
-      }else{
-         for(j=n;j>=1;j--)
-         {
-          c=a+b;
-          arr[i][j]=c;
-          a=b;
-          b=c;
-         }
-      }
-     }
-     for(i=1;i<=n;i++)
-     {
-        for(j=1; j<=n; j++)
-        cout<<arr[i][j]<<" ";
-     cout<<endl;
-     }
+#include <iostream> 
+using namespace std; 
+int a[102][102], n;
+int main(){ 
+  cin >> n;
+  a[1][n/2 + 1] = a[n][n/2+1] = 1;
+  for(int i = 2 , j = n - 1 ; i <= j ; i ++, j --) { 
+    a[i][n/2+1] = a[i-1][n/2+1] + 1; 
+    a[j][n/2+1] = a[j+1][n/2+1] + 1; 
+      for(int k = a[i][n/2+1]-1 , p = 1 ; k > 0 ; k -- , p ++)
+        a[i][n/2+1-p] = a[i][n/2+1+p] = k; 
+    for(int k = a[j][n/2+1]-1 , p = 1 ; k > 0 ; k -- , p ++)
+      a[j][n/2+1-p] = a[j][n/2+1+p] = k; } 
+  for (int i = 1 ;i <= n ; ++i) { 
+    for(int j = 1 ; j <= n ; ++j)
+      cout << a[i][j] << " "; 
+    cout << endl; 
+  } 
+  return 0;
 }
