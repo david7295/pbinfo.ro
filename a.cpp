f1f102adaf18e1ebf4e397b8a7a4df35bb9b8eb5
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-04 17:46:44 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,56 +1,27 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-
-{
-
-int n;
-
-cin>>n;
-
-int M[100][100];
-
-for(int i=1; i<=n; i++)
-
-for(int j=1; j<=n; j++)
-
-{
-
-if(i==1 || i==n || j==1 || j==n)
-
-M[i][j]=i+j;
-
-}
-
-
-for(int j=n-1; j>1; j--)
-
-for(int i=2; i<=n-1; i++)
-
-{
-
-M[i][j]=M[i-1][j+1]+M[i][j+1]+M[i+1][j+1];
-
-}
-
-
-for(int i=1; i<=n; i++)
-
-{for(int j=1; j<=n; j++)
-
-{
-
-cout<<M[i][j]<<" ";
-
-}
-
-cout<<endl;
-
-}
-
-
-return 0;
-
+long int a[102][102];
+int main(){   
+  long int n,i,j,x=1,y=2;  
+  cin>>n;  
+  for(i=1;i<=2*n;i++)    {  
+    if(i%2==1) {  
+      for(j=1;j<=n*2;j++)  {     
+        a[i][j]=x;      
+        x+=2;
+      }       
+    }       
+    else {    
+      for(j=n*2;j>=1;j--)  { 
+        a[i][j]=y;          
+        y+=2;       
+      }      
+    }  
+  }   
+  for(i=1;i<=n*2;i++)    { 
+    for(j=1;j<=n*2;j++)  
+      cout<<a[i][j]<<' ';   
+    cout<<'\n';  
+  }  
+  return 0;
 }
