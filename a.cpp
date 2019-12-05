19ad35a08dc5b9c8cb71b707e199a77212fdaafc
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-05 15:12:18 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,63 +1,19 @@
-#include <iostream>
+#include<fstream>
+#include<algorithm>
+using namespace std;
 
-using namespace std;
+ifstream f("xyz.in");
+ofstream g("xyz.out");
 
-int n, i, v[2502], a1,d;
-
-int main()
-
+int main()
 {
-
-   cin >> n;
-
-   for (i=0; i<n; ++i)
-
-       cin >> v[i];
-
-   if (n==1) cout << "DA";
-
-   else
-
-   {
-
-       a1=v[0]; d=v[1]-v[0];
-
-       if (d<0) d=-d;
-
-       a1=min(v[0],v[1]);
-
-       int ratia=d;
-
-       for (i=2; i<n; ++i)
-
-       {
-
-           d=v[i]-v[i-1];
-
-           if (d<0) d=-d;
-
-           if (d<ratia) ratia=d;
-
-           a1=min(a1,v[i]);
-
-       }
-
-       int este=1;
-
-       for (i=0; i<n; ++i)
-
-       {
-
-           if ((v[i]-a1)%ratia!=0) {este=0; break;}
-
-       }
-
-       if (este) cout << "DA";
-
-       else cout << "NU";
-
-   }
-
-   return 0;
-
+int x,y,z,i,nr;
+f>>x>>y>>z;
+nr=y;
+for(i=1;i<x;i++)
+nr=nr*10+z;
+g<<nr;
+f.close();
+g.close();
+return 0;
 }
