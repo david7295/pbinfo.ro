cd699b6714bc547841fb888d30dea912d364ea47
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-05 22:05:50 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,47 +1,20 @@
-#include <iostream>
-
+#include<iostream>
+#include<fstream>
 using namespace std;
-
-int n, i, m[11], b, r, cif, num[11], r2, j;
-
-int main()
-
-{
-
-   cin >> n;
-
-   for (i=1; i<=n; ++i)
-
-   {
-
-       cin >> m[i] >> b >> r;
-
-       r2=1;
-
-       for (j=1; j<m[i]; ++j)
-
-           r2=(r2*10)%b;
-
-       if (r>=r2) cif=r-r2;
-
-       else cif=b+r-r2;
-
-       num[i]=cif;
-
-   }
-
-   for (i=1; i<=n; ++i)
-
-   {
-
-       cout << 1;
-
-       for (j=1; j<=m[i]-2; ++j)
-
-           cout << 0;
-
-       cout << num[i] << endl;
-
-   }
-
+ int main(){
+  ifstream in("max_suma.in");
+  ofstream out("max_suma.out");
+  int n,m,a[50],maxim,suma;
+  in>>n>>m;
+  for (int cont=1; cont<=n;cont++ ){ 
+    for  (int i=0; i<m; i++){      
+      in>>a[i];}
+  suma = 0;
+  maxim = a[0];
+  for (int i=0; i<m; i++){   
+    suma+= a[i];   
+  if (a[i]>maxim)     
+    maxim = a[i];
+ }
+ out<<suma<<" "<<maxim<<endl;}
 }
