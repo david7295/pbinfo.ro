a26be9fd8485e9b75685c834b6396d118c7356f9
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-05 18:31:43 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,75 +1,11 @@
-#include <fstream>
-
+#include <iostream>
 using namespace std;
 
-ifstream fin("pagini.in");
-
-ofstream fout("pagini.out");
-
-int n, i, x, T, impare, xx, cx;
-
-int F[10];
-
-int main()
-
-{
-
-   fin >> T;
-
-   while (T)
-
-   {
-
-       fin >> n;
-
-       for (i = 0; i < 10; i++) F[i] = 0;
-
-       for (i = 1; i <= n; i++)
-
-       {
-
-           fin >> x;
-
-           for (xx = 1; xx <= x; ++xx)
-
-  {
-
-   cx = xx;
-
-   while (cx)
-
-   {
-
-    F[cx % 10]++;
-
-    cx /= 10;
-
-   }
-
-  }
-
-       }
-
-       impare = 0;
-
-       for (i = 0; i < 10; i++)
-
-           if (F[i] % 2)
-
-               impare++;
-
-       if (impare > 1)
-
-           fout << "NU\n";
-
-       else
-
-           fout << "DA\n";
-
-       T--;
-
-   }
-
-   return 0;
-
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << y;
+    for(int i = 1; i < x; i++)
+      cout << z;
+    return 0;
 }
