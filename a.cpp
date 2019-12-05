a3d779b646187a2f77b7e5c0db78c8f4a55a8e1d
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-05 16:33:23 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,93 +1,75 @@
-#include <iostream>
-
 #include <fstream>
 
 using namespace std;
 
-short fr[201], n, i, ap[201], k, num, nrcontrol;
+ifstream fin("pagini.in");
 
-ifstream f("control2.in");
+ofstream fout("pagini.out");
 
-ofstream g("control2.out");
+int n, i, x, T, impare, xx, cx;
 
-bool prim(short m)
-
-{
-
-   short p=1;
-
-   if (m==0 || m==1) p=0;
-
-   if (m==2) p=1;
-
-   if (m>2)
-
-   {
-
-      if (m>2)
-
-      {
-
-          if (m%2==0) p=0;
-
-          for (int j=3; j*j<=m; ++j)
-
-          {
-
-              if (m%j==0) { p=0; break; }
-
-          }
-
-      }
-
-   }
-
-   if (p) return 1;
-
-   else return 0;
-
-}
+int F[10];
 
 int main()
 
 {
 
-  f >> n;
+   fin >> T;
 
-  for (i=1; i<=n; ++i)
+   while (T)
 
-  {
+   {
 
-      f >> num; ++fr[num];
+       fin >> n;
 
-  }
+       for (i = 0; i < 10; i++) F[i] = 0;
 
-  for (i=0; i<=200; ++i)
+       for (i = 1; i <= n; i++)
 
-  {
+       {
 
-     if (fr[i]) { ++k; ap[k]=fr[i]; }
+           fin >> x;
 
-  }
-
-  while (k%3) --k;
-
-  short unu=0, zero=0;
-
-  for (i=1; i<=k-2; i+=3)
+           for (xx = 1; xx <= x; ++xx)
 
   {
 
-      if ((ap[i]%2 && ap[i+1]%2 && ap[i+2]%2) || (ap[i]%2==0 && ap[i+1]%2==0 && ap[i+2]%2==0)) ++unu;
+   cx = xx;
 
-      else ++zero;
+   while (cx)
+
+   {
+
+    F[cx % 10]++;
+
+    cx /= 10;
+
+   }
 
   }
 
-  nrcontrol=unu*10+zero;
+       }
 
-  g << nrcontrol << "\n";
+       impare = 0;
 
-  g << prim(nrcontrol);
+       for (i = 0; i < 10; i++)
+
+           if (F[i] % 2)
+
+               impare++;
+
+       if (impare > 1)
+
+           fout << "NU\n";
+
+       else
+
+           fout << "DA\n";
+
+       T--;
+
+   }
+
+   return 0;
 
 }
