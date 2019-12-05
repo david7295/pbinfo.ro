eddfda453fcfd05b3e610644ec4b4231f431f3fe
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-05 07:27:51 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,38 +1,63 @@
-#include<bits/stdc++.h>
-using namespaxe std;
-int a[30][30],i,j,n;
-void matrice(int x)
-{
-int k=1,l=1;
-int y=x;
-for(int m=1;m<=x;m++)
-{
-for(i=1;i<=x;i++)
-{
-for(j=1;j<=x;j++)
-{
-a[x-j+k][j]=y;
-a[x-j+l][j]=y;
-}
-}
-k++;
-l--;
-y--;
-}
-}
-void afis()
-{
-for(i=1;i<=n;i++)
-{
-for(j=1;j<=n;j++)
-cout<<a[i][j]<<" ";
-cout<<endl;
-}
-}
+#include <iostream>
+
+using namespace std;
+
+int n, i, v[2502], a1,d;
+
 int main()
+
 {
-cout<<"dati n: ";cin>>n;
-matrice(n);
-afis();
-return 0;
+
+   cin >> n;
+
+   for (i=0; i<n; ++i)
+
+       cin >> v[i];
+
+   if (n==1) cout << "DA";
+
+   else
+
+   {
+
+       a1=v[0]; d=v[1]-v[0];
+
+       if (d<0) d=-d;
+
+       a1=min(v[0],v[1]);
+
+       int ratia=d;
+
+       for (i=2; i<n; ++i)
+
+       {
+
+           d=v[i]-v[i-1];
+
+           if (d<0) d=-d;
+
+           if (d<ratia) ratia=d;
+
+           a1=min(a1,v[i]);
+
+       }
+
+       int este=1;
+
+       for (i=0; i<n; ++i)
+
+       {
+
+           if ((v[i]-a1)%ratia!=0) {este=0; break;}
+
+       }
+
+       if (este) cout << "DA";
+
+       else cout << "NU";
+
+   }
+
+   return 0;
+
 }
