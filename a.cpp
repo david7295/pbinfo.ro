11574b0ab324400f31a379d113ee7d89200f5076
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-06 07:27:18 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,32 +1,53 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <cassert>
 using namespace std;
-#define NN 1005
 
-ifstream fin("plopi1.in");
-ofstream fout("plopi1.out");
+ifstream fin ("euler.in");
+ofstream fout("euler.out");
 
-int n, a[NN], L[NN];
+int n , a[205][205], d[205], x[50000], y[50000], p , q;
 
 
-int main(){
-assert(fin >> n );
-for(int i=1 ; i<=n ; ++i)
-assert(fin >> a[i]);
-L[n] = 1;
-for(int i=n-1 ; i>0 ; i--)
+int main()
 {
-L[i] = 1;
-for(int j=i+1 ; j<=n; ++j)
-if(a[i]>a[j] && L[i]<L[j]+1)
-L[i] = L[j] + 1;
+    int i , j;
+    fin >> n;
+    while(fin >> i >> j)
+    {
+a[i][j] = a[j][i] = 1;
+d[i] ++, d[j] ++;
+    }
+    p = 1; x[1] = 1;
+    while(1)
+    {
+int xpoz = 0;
+for(int i = 1 ; i <= p && xpoz == 0 ; ++i)
+if(d[x[i]] > 0)
+xpoz = i;
+if(xpoz == 0)
+break;
+q = 1; y[1] = x[xpoz];
+do
+{
+int ypoz = 0;
+for(int i =1 ; i <= n && ypoz == 0 ; i ++)
+if(a[y[q]][i] != 0)
+ypoz = i;
+d[y[q]] --;
+d[ypoz] --;
+a[y[q]][ypoz] = a[ypoz][y[q]] = 0;
+y[ ++ q] = ypoz;
 }
-int pmax = 1;
-for(int i=1 ; i<=n ; ++i)
-if(L[pmax] <= L[i])
-pmax = i;
-fout << n - L[pmax] << endl;
+while(y[q] != y[1]);
+for(int i = p; i > xpoz ; i --)
+x[i + q - 1] = x[i];
+for(int i = xpoz, j = 1 ; j <= q ; i ++, j ++)
+x[i] = y[j];
+p += q - 1;
+}
+fout << p << "\n";
+for(int i =1 ; i <= p ; i ++)
+fout << x[i] << " ";
 return 0;
 }
