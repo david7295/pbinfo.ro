45be7a4bd459d7faa943b9666027e96b4ed5d744
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-08 15:16:58 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,53 +1,107 @@
-#include <iostream>
-#include <fstream>
-#include <cassert>
-using namespace std;
+#include <iostream>
 
-ifstream fin ("euler.in");
-ofstream fout("euler.out");
+#include <string>
 
-int n , a[205][205], d[205], x[50000], y[50000], p , q;
+using namespace std;
 
+int main()
 
-int main()
 {
-    int i , j;
-    fin >> n;
-    while(fin >> i >> j)
-    {
-a[i][j] = a[j][i] = 1;
-d[i] ++, d[j] ++;
-    }
-    p = 1; x[1] = 1;
-    while(1)
-    {
-int xpoz = 0;
-for(int i = 1 ; i <= p && xpoz == 0 ; ++i)
-if(d[x[i]] > 0)
-xpoz = i;
-if(xpoz == 0)
-break;
-q = 1; y[1] = x[xpoz];
-do
-{
-int ypoz = 0;
-for(int i =1 ; i <= n && ypoz == 0 ; i ++)
-if(a[y[q]][i] != 0)
-ypoz = i;
-d[y[q]] --;
-d[ypoz] --;
-a[y[q]][ypoz] = a[ypoz][y[q]] = 0;
-y[ ++ q] = ypoz;
+
+char pattern[256], modelul[256], nr[3], sir[50], text[100];
+
+int i, j, numar, paranteze=0, p1, p2, pnr, x;
+
+cout<<"pattern:"; cin>>pattern;
+
+for(i=0;i<strlen(pattern);i++)
+
+ if(pattern[i]=='[' || pattern[i]==']')
+
+  paranteze++;
+
+while(paranteze){
+
+ p1=0; p2=0;
+
+ for(i=0;i<strlen(pattern);i++)
+
+  if(pattern[i]=='['){
+
+   p1++;
+
+   if(p1==paranteze/2){
+
+    p1=i; break;
+
+   }
+
+  }
+
+ for(i=p1;i<=strlen(pattern);i++)
+
+  if(pattern[i]==']'){
+
+   p2=i; break;
+
+  }
+
+ i=p1-1;
+
+ while(pattern[i]>='0' && pattern[i]<='9' && i>0)
+
+  i--;
+
+ if(i==0) pnr=0;
+
+ else pnr=i+1;
+
+ x=0;
+
+ if(i==0) i=-1;
+
+ for(j=i+1;j<p1;j++){
+
+  nr[x]=pattern[j]; x++;}
+
+ nr[x]='\0';
+
+ numar=atoi(nr);
+
+ x=0;
+
+ for(i=p1+1;i<p2;i++){
+
+  sir[x]=pattern[i]; x++;
+
+ }
+
+ sir[x]='\0';
+
+ numar--;
+
+ strcpy(text,pattern+p2+1);
+
+ while(numar){
+
+  numar--;
+
+  strcpy(pattern+p2,sir);
+
+  p2=p2+strlen(sir);
+
+ }
+
+ strcpy(pattern+p2,text);
+
+ strcpy(pattern+pnr,pattern+p1+1);
+
+ paranteze=paranteze-2;
+
 }
-while(y[q] != y[1]);
-for(int i = p; i > xpoz ; i --)
-x[i + q - 1] = x[i];
-for(int i = xpoz, j = 1 ; j <= q ; i ++, j ++)
-x[i] = y[j];
-p += q - 1;
-}
-fout << p << "\n";
-for(int i =1 ; i <= p ; i ++)
-fout << x[i] << " ";
-return 0;
+
+cout<<pattern;
+
+return 0;
+
 }
