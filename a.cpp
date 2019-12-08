34f945cdef78c299ffd07b42897894148186e2f1
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-08 19:38:20 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,120 +1,37 @@
-#include <fstream>
+#include <iostream>
 
-using namespace std;
+#include <fstream>
 
-ifstream fin("accesibil.in");
-ofstream fout("accesibil.out");
+using namespace std;
 
-int accesibil(long long n);
-int caccesibil(long long n);
-int nrcifre(long long n);
+int main()
 
-int main()
 {
 
-    int p,k,n;
-    fin >> p >> k >> n;
+   ifstream fin("numar4.in");
 
-    if(p==1)
-    {
-        long long a=-1,b=-1,c=-1,x;
-        for(;n;n--)
-        {
-            fin >> x;
-            if(accesibil(x) and x>=c)
-            {
-                c=x;
-                if(c>=b)
-                    swap(c,b);
-                if(b>=a)
-                    swap(a,b);
-            }
-        }
-        fout << c << " " << b << " " << a;
-    }
-    if(p==2)
-    {
-        int nr=0;
-        long long x;
-        for(;n;n--)
-        {
-            fin >> x;
-            if(!accesibil(x) and caccesibil(x))
-                nr++;
-        }
-        fout << nr;
-    }
-    if(p==3)
-    {
-        long long Min=0,Max=0,q=1;
-        if(k==9)
-            fout << 123456789;
-        else
-        {
-            for(int i=1;i<=k;i++)
-            {
-                Min=Min*10+i;
-                Max+=(10-i)*q;
-                q*=10;
-            }
-            fout << Min << " " << Max;
-        }
-    }
-    if(p==4)
-        fout << (5-(k+1)/2) << " " << (5-k/2);
+   ofstream fout("numar4.out");
 
-    return 0;
+   int n;
 
-}
-int accesibil(long long n)
-{
-    if(n<10)
-        return 0;
-    int k=n%10,c;
-    while(n>9)
-    {
-        n/=10;
-        c=n%10;
-        if(c+1!=k)
-            return 0;
-        else
-            k=c;
-    }
-    return 1;
-}
-int caccesibil(long long n)
-{
-    if(n<100)
-        return 0;
-    int nr=nrcifre(n);
-    for(;nr;nr--)
-    {
-        int q=0;
-        long long x=0,p=n,k=1;
-        while(p!=0)
-        {
-            q++;
-            if(q!=nr)
-            {
-                x+=k*(p%10);
-                k*=10;
-            }
-            p/=10;
-        }
-        if(accesibil(x))
-            return 1;
-    }
-    return 0;
-}
-int nrcifre(long long n)
-{
-    if(n<10)
-        return 1;
-    int nr=0;
-    while(n!=0)
-    {
-        n/=10;
-        nr++;
-    }
-    return nr;
+   fin >> n;
+
+   int nr=0;
+
+   while (n)
+
+   {
+
+       if (n%2==0) nr+=4;
+
+       else nr+=5;
+
+       n/=10;
+
+   }
+
+   fout << nr;
+
+   return 0;
+
 }
