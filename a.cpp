f3c7bcebed8725365bbea6cd099e67143a1bb9d5
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-11 21:38:52 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
-
-int main()
-
-{
-
-int n,b;
-
-long long int x;
-
-cin>>n;
-
-for(int i = 0; i < n;i++){
-
-cin >> x >> b;
-
-cout << ((x & (1<<b))>>b) <<"\n";
-
-}
-
+int main(){   
+  char s[256],ma[128]=""; 
+  cin.get(s,256); 
+  for(unsigned int i=0;i<strlen(s);i++)  
+    for(unsigned int j=i+1;j<strlen(s);j++)    { 
+      char cuv[256]="";   
+      strncat(cuv,s+i,j-i);   
+      if(strstr(s+j,cuv)!=0)   
+        if(strlen(cuv)>strlen(ma)||strlen(cuv)==strlen(ma)&&strcmp(cuv,ma)<0)      
+          strcpy(ma,cuv); }  
+  cout<<ma;   
+  return 0;
 }
