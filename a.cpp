96edc5b40ba45cd03070183b3c590f3316dc1a08
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-12 17:36:10 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,22 +1,49 @@
-#include <iostream> 
+#include <iostream>
+
 using namespace std;
-int main() { 
-  long long v[10000]={0},i,n,x,k1=0,k2=0,ma=-1;
-  cin>>n; 
-  for(i=1;i<=n;i++) {
-    cin>>x;
-    if(x>ma) 
-      ma=x;
-    v[x]++; }
-  for(i=1;i<=ma;i++) {
-    if(v[i]%2==1) k1++;
-    if(v[i]%2==0 && v[i]!=0)
-      k2++; 
-  } 
-  if(k1==0 || k1==1)
-    cout<<"DA";
-  else 
-    cout<<"NU"; 
-  return 0; 
+
+int n, S, i, j, p[1002], imin, imax, M, ping, dif, difmax;
+
+int main()
+
+{
+
+   cin >> n >> S;
+
+   for (i=1; i<=n; ++i)
+
+       cin >> p[i];
+
+   difmax=p[2]-p[1]; imin=1; imax=2;
+
+   for (i=1; i<n; ++i)
+
+   {
+
+       for (j=i+1; j<=n; ++j)
+
+       {
+
+           dif=p[j]-p[i];
+
+           if (dif>difmax)
+
+           {
+
+               difmax=dif;
+
+               imin=i; imax=j;
+
+           }
+
+       }
+
+   }
+
+   ping=S/p[imin];
+
+   M=S%p[imin] + ping*p[imax];
+
+   cout << M;
+
 }
-\
