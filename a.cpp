c684ee63f7c5a59461a5323ea89fae7e0bca5302
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-12 17:32:22 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
-#include <cstring>
+#include <iostream> 
 using namespace std;
-int main(){   
-  char s[256],ma[128]=""; 
-  cin.get(s,256); 
-  for(unsigned int i=0;i<strlen(s);i++)  
-    for(unsigned int j=i+1;j<strlen(s);j++)    { 
-      char cuv[256]="";   
-      strncat(cuv,s+i,j-i);   
-      if(strstr(s+j,cuv)!=0)   
-        if(strlen(cuv)>strlen(ma)||strlen(cuv)==strlen(ma)&&strcmp(cuv,ma)<0)      
-          strcpy(ma,cuv); }  
-  cout<<ma;   
-  return 0;
+int main() { 
+  long long v[10000]={0},i,n,x,k1=0,k2=0,ma=-1;
+  cin>>n; 
+  for(i=1;i<=n;i++) {
+    cin>>x;
+    if(x>ma) 
+      ma=x;
+    v[x]++; }
+  for(i=1;i<=ma;i++) {
+    if(v[i]%2==1) k1++;
+    if(v[i]%2==0 && v[i]!=0)
+      k2++; 
+  } 
+  if(k1==0 || k1==1)
+    cout<<"DA";
+  else 
+    cout<<"NU"; 
+  return 0; 
 }
+\
