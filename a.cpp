f85e11289eea8a6c06f82bea271ec16ef74eddbe
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-13 17:51:44 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+
 using namespace std;
-int main() {
-  int n,Z,A[201][201],S=0;
-  cin>>n>>Z;
-  for(int i=1;i<=n;i++) 
-    for(int j=1;j<=n;j++) 
-      cin>>A[i][j];
-  if(Z==1)
-    for(int i=1;i<=n;i++)
-      for(int j=1;j<=n;j++)
-        if(i<j&&i+j<n+1)
-          S+=A[i][j];
-  if(Z==2) 
-    for(int i=1;i<=n;i++) 
-      for(int j=1;j<=n;j++)
-        if(i<j&&i+j>n+1)
-          S+=A[i][j];
-  if(Z==3)
-    for(int i=1;i<=n;i++)
-      for(int j=1;j<=n;j++)
-        if(i>j&&i+j>n+1) 
-          S+=A[i][j];
-  if(Z==4)
-    for(int i=1;i<=n;i++)
-      for(int j=1;j<=n;j++)
-        if(i>j&&i+j<n+1)
-          S+=A[i][j];
-  cout<<S; 
-  return 0;
+int i,contor=0,contor2=0,k=0;
+char a;
+string condensat,necondensat;
+int main()
+{
+    cin>>condensat;
+     for(i=0;i<condensat.size();i++)
+     {
+         if(condensat[i]=='(')
+            contor++;
+         if(condensat[i]==')')
+            contor++;
+     }
+     int pozitii[contor];
+     for(i=0;i<condensat.size();i++)
+     {
+         if(condensat[i]=='(')
+         {
+             pozitii[contor2]=i;
+             contor2++;
+         }
+         if(condensat[i]==')')
+         {
+             pozitii[contor2]=i;
+             contor2++;
+         }
+     }
+     for(i=0;i<contor/2;i++)
+     {
+         if(isdigit(condensat[pozitii[k+1]+1]))
+         {
+             string a=condensat.substr(pozitii[k+1]+1,1);
+             for(int j=0;j<atoi(a.c_str());j++)
+             {
+                 necondensat+=condensat.substr(pozitii[k]+1,pozitii[k+1]-pozitii[k]-1);
+             }
+         }
+         else
+         {
+             necondensat+=condensat.substr(pozitii[k]+1,pozitii[k+1]-pozitii[k]-1);
+         }
+         k+=2;
+     }
+     cout<<necondensat;
+    return 0;
 }
