11360fc855d581c5b9779b2341d90834caaf43c7
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-13 23:46:17 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,45 +1,31 @@
-#include <fstream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int f[1000000];
-
-int main(){
-
-int n,x, m= 0;
-
-ifstream fin("mmult.in");
-
-ofstream fout("mmult.out");
-
-fin>>n;
-
-for(int i = 0; i < n; i++){
-
-fin >> x;
-
-if(x > m) m = x;
-
-f[x]++;
-
+int t, i, a, b, j, nr, dif, v[1000005];
+void ciur(){  
+  for(i=2; i<1000000; i++)     
+    if(!v[i])  {  
+      dif=1; 
+      for(j=i; j<=1000000; j+=i)  {   
+        if(j%(i*i)==0)   {    
+          dif++;     
+          v[j]+=dif;        
+        }  
+        else v[j]++;   
+      }    
+    }
 }
-
-fin.close();
-
-int r = f[1];
-
-int prev = f[1];
-
-for(int i = 2; i <= m; i++){
-
-if(f[i] > prev){r = -1; break;}
-
-prev = f[i];
-
-}
-
-fout << r;
-
-fout.close();
-
+int suma[1000002];
+int main(){  
+  ciur();  
+  for(i=1;i<=1000001;++i)   
+  {
+    suma[i]=suma[i-1];   
+    if(v[i]==2) ++suma[i];
+  }  
+  cin >> t;   
+  for(i=1; i<=t; i++)    {   
+    cin >> a >> b;   
+    cout << suma[b]-suma[a-1]<< " ";  
+  } 
+  return 0;
 }
