4a8a9d6b12edc19e0ba7e68addc1e5b96c251c56
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-13 15:55:16 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,23 +1,36 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-
+#include <bits/stdc++.h>
+using namespace std;
+int v[100005], n, x, y, nr = 1, i, p, k, lmax;
+int main()
 {
-
-   int N; cin >> N;
-
-   for (int i = 1; i<= N; i++)
-
-   {
-
-       unsigned long long x; cin  >> x;
-
-       cout << x%7 << '\n';
-
-   }
-
-   return 0;
-
+   ifstream f("sir6.in");
+   ofstream g("sir6.out");
+   f >> p >> n;
+   if(n == 1) lmax = 1;
+   f >> y;
+   v[++ k] = y;
+   for(i = 2; i <= n; i ++)
+     {
+         f >> x;
+         if(x == y)
+         {
+             nr ++;
+             if(nr > lmax) lmax = nr;
+         }
+          else
+         {
+              v[++ k] = nr;
+              v[++ k] = x;
+              nr = 1;
+         }
+         y = x;
+     }
+     v[++ k] = nr;
+     if(p == 1) g << lmax;
+     if(p == 2)
+     {
+         for(i = 1; i <= k; i ++)
+          g << v[i] << " ";
+     }
+    return 0;
 }
