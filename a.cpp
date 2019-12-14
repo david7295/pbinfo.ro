f91a2ee13b71adbb69fc8433a4516cb020daeeba
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-14 20:07:26 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,31 +1,129 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-int t, i, a, b, j, nr, dif, v[1000005];
-void ciur(){  
-  for(i=2; i<1000000; i++)     
-    if(!v[i])  {  
-      dif=1; 
-      for(j=i; j<=1000000; j+=i)  {   
-        if(j%(i*i)==0)   {    
-          dif++;     
-          v[j]+=dif;        
-        }  
-        else v[j]++;   
-      }    
-    }
-}
-int suma[1000002];
-int main(){  
-  ciur();  
-  for(i=1;i<=1000001;++i)   
-  {
-    suma[i]=suma[i-1];   
-    if(v[i]==2) ++suma[i];
-  }  
-  cin >> t;   
-  for(i=1; i<=t; i++)    {   
-    cin >> a >> b;   
-    cout << suma[b]-suma[a-1]<< " ";  
-  } 
-  return 0;
+
+int main()
+
+{
+
+   int n = 19, nr = 3;  //citeste din fisier N si NR
+
+   //Citeste cele N numere de pe linia 2
+
+   int v[n] = {1, 0, -1, 0, -1, 1, 0, -1, 1, 1, -1, 1, 0, 1, -1, 1, 0, 1, -1};
+
+   int ap[100];
+
+   
+
+   //initializam elementele din vectorul de numarare cu 0
+
+   for(int j=0;j<100;j++)
+
+   {
+
+       ap[j] =0;
+
+   }
+
+   //transformam din binare in zecimale
+
+   string nrTemp="";
+
+   for(int i = 0;i<n;i++)
+
+   {
+
+       if(v[i] != -1)
+
+           nrTemp+=v[i];
+
+       else
+
+       {
+
+           
+
+           int cnt = nrTemp.length()-1;
+
+           int nr = 0;
+
+           int pow = 1;
+
+           
+
+           for(int z = nrTemp.length()-1;z>=0;z--)
+
+           {
+
+               pow = 1;
+
+               for(int j =0;j<cnt-z;j++)
+
+                       pow*=2;
+
+                       
+
+               nr+=(int)(nrTemp[z]) * pow;
+
+           }
+
+           ap[nr] ++;
+
+           nrTemp="";
+
+       }
+
+   }
+
+   
+
+   //vedem care este numarul maxim de aparitii
+
+   int maxAparitie=0;
+
+   
+
+   for(int j=100;j>=0;j--)
+
+       {
+
+          if(maxAparitie < ap[j])
+
+               maxAparitie = ap[j];
+
+       }
+
+   
+
+   //Afisam primele nr numere descrescator dupa nr de aparitie si descrescator daca au acelasi nr de aparitie
+
+   int cnt=0;
+
+   for(int i = maxAparitie;i>=1 && cnt<nr;i--)
+
+  {
+
+       for(int j=99;j>=0 && cnt<nr;j--)
+
+       {
+
+          if(ap[j] == i)
+
+          {
+
+               cout<<j<<" "<<ap[j]<<endl;
+
+               cnt++;
+
+          }
+
+       }
+
+   }
+
+   
+
+   return 0;
+
 }
