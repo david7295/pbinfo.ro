d46818040d46b4c09937f11e3f2f008fc182af9f
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-15 00:41:16 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 
+#include <cmath>
+
 using namespace std;
 
-const int MAX_N = 10000;
+int main() {
 
-int n, x, y;
-bool apartine[1 + MAX_N] ;
+int n, curent;
 
-void genereaza(int b) {
-  if (b <= n && !apartine[b]) {
-    apartine[b] = true;
-    genereaza(b + x);
-    genereaza(b + y);
-  }
+cin >> n;
+
+while (n > 0) {
+
+ for (int i = 0; i <= n; i++) {
+
+  if (n >= pow(2, i))
+
+   curent = pow(2, i);
+
+ }
+
+ cout << endl << curent;
+
+ n = n - curent;
+
 }
 
-int main() {
-  int a;
-  cin >> n >> a >> x >> y;
-  genereaza(a);
-  for (int i = 0; i <= n; i++)
-    if (apartine[i])
-      cout << i << ' ';
-  return 0;
 }
