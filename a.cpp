5e0d840bd8c3cca76f5a73571c59c316ca6137e0
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-14 22:07:31 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,45 +1,30 @@
-#include <fstream>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
-using namespace std;
-
-char diag_principala[3000];
-
-char diag_secundara[3000];
-
-int main(){
-
- ifstream fin("decodificare.in");
-
- ofstream fout("decodificare.out");
-
-   
-
- int n;
-
- fin >> n;
-
- char c;
-
- for(int i = 0; i < n/2; i++){
-
-   for(int j = 0; j < n/2; j++){
-
-     fin >> c;
-
-     if(i == j)diag_principala[i] = c;
-
-     if(i + j == (n-1)/2)diag_secundara[i] = c;
-
-   }
-
- }
-
- fin.close();
-
- fout << diag_secundara << diag_principala;
-
- fout.close();
-
- return 0;
-
+int main(){
+int L,H,X,Y,latime,inaltime,nr_patrate=0;
+ifstream fip("patrate1.in");
+ofstream fop("patrate1.out");
+fip>>L>>H>>X>>Y;
+latime=X/L;
+inaltime=Y/H;
+while(latime>0||inaltime>0){
+if(latime>0&&inaltime>0){
+latime--;
+inaltime--;
+nr_patrate++;
+}
+else if(latime>0){
+latime--;
+nr_patrate++;
+}
+else if(inaltime>0){
+inaltime--;
+nr_patrate++;
+}
+}
+nr_patrate++;
+fop<<nr_patrate;
+return 0;
 }
