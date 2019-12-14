194d9e1364d7fdb08f04a5e92d623914efc3b646
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-14 21:23:08 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,47 +1,25 @@
-#include <iostream>
-
-#include <bitset>
-
-#include <fstream>
-
-using namespace std;
-
-int b[100001];
-
-int n, i, j, secv, secvmax, nr1, nrswap,bit, rep;
-
-int main()
-
-{
-
-  cin >> n;
-
-  for (i=0; i<n; ++i)
-
-  {
-
-      cin >> b[i];
-
-      nr1+=b[i];
-
-  }
-
-  for (j=0; j<nr1; ++j) secv+=b[j];
-
-  secvmax=secv;
-
-  for (i=nr1; i<n; ++i)
-
-  {
-
-      secv=secv-b[i-nr1]+b[i];
-
-      if (secv>secvmax) secvmax=secv;
-
-  }
-
-  nrswap=nr1-secvmax;
-
-  cout << nrswap;
-
+#include<iostream>
+int n,m,p,v[10001],s,u[10001];
+int main(){  
+  std::cin>>n>>p;  
+  for(int i=1;i<=n;++i)    {  
+    int a;   
+    std::cin>>a;   
+    v[a]++;   
+  }   
+  std::cin>>m; 
+  for(int i=1;i<=m;++i)    {  
+    int a;   
+    std::cin>>a;    
+    u[a]++;  
+  }  
+  for(int i=0;i<10000;++i)    {
+    if(v[i]!=0)     
+      for(int j=0;j<10000;++j)    {     
+        if(i*j>=p)       
+          break;      
+        s+=v[i]*u[j];    
+      }   
+  }  
+  std::cout<<s;
 }
