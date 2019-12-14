34c2d91d1620495009ad21f1544476c16c1b5fd6
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-14 20:54:05 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,75 +1,17 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
-
-int main()
-
-{
-
-ifstream fin("key.in");
-
-ofstream fout("key.out");
-
-int p, n, sum = 0, v[4]{}, cost;
-
-string mk, s;
-
-fin >> p >> mk >> n;
-
-do
-
-{
-
- fin >> s >> cost;
-
- if(s == mk)
-
-  ++v[3];
-
- else if(s[0] == mk[0] && s[1] == mk[1] ||
-
-   s[0] == mk[0] && s[2] == mk[2] ||
-
-   s[1] == mk[1] && s[2] == mk[2])
-
- {
-
-  ++v[2];
-
-  sum += cost / 3;
-
- }
-
- else if(s[0] == mk[0] || s[1] == mk[1] || s[2] == mk[2])
-
- {
-
-  ++v[1];
-
-  sum += cost / 3 * 2;
-
- }
-
- else
-
- {
-
-  ++v[0];
-
-  sum += cost;
-
- }
-
-} while(--n);
-
-if(p == 1)
-
- fout << v[0] << ' ' << v[1] << ' ' << v[2] << ' ' << v[3];
-
-else if(sum == 0)
-
- fout << "GRATIS";
-
-else fout << sum;
-
+int main(){  
+  int n,k,x,nrelimp=0,c=1;
+  cin >> n >> k;   
+  for (int i = 1; i<=n; i++) {  
+    cin >> x;  
+    if (x%2)      
+      nrelimp++;   
+    if (nrelimp > k)  { 
+      c++;    
+      nrelimp = 1;   
+    }   
+  }  
+  cout << c;
+  return 0;
 }
