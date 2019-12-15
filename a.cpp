e35f3f3dfb52ee31d447390c53965c68135e1da7
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-15 14:23:46 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,12 +1,35 @@
-#include <iostream>
-#include <algorithm> //libraria pentru functia sort()
+#include <iostream>
+#include <fstream>
+using namespace std;
 
-using namespace std;
-
-int main()
+int main()
 {
-    string s = "dacb";
-    sort(s.begin(), s.end());
-    cout << s <<endl;
-    return 0;
+    int n,x,i=0,j,k,C=0,ok=0;
+    ifstream fin("ssume.in");
+    fin>>n;
+    int v[n];
+    while(fin>>x)
+    {
+        v[i]=x;
+        i++;
+    }
+    for(i=0;i<n;i++)
+    {
+        ok=0;
+        for(j=0;j<n;j++)
+            if(i!=j)
+            {
+            for(k=j+1;k<n;k++)
+              if(v[i]==v[j]+v[k] && !ok)
+               {
+                 C++;
+                 ok=1;
+               }
+            }
+    }
+    ofstream fout("ssume.out");
+    fout<<C;
+    fin.close();
+    fout.close();
+  return 0;
 }
