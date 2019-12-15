779b1d67489040021a0398bfaeb95795142f0c8c
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-15 08:31:53 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,23 +1,33 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
+#include <iostream>
 
-long int factorial(int n){
-long int i,fact=1;
-for(i=2;i<=n;i++){
-fact=fact*i;
-}
-return fact;
-}
-int main(){
-long int n,p,i,k=1,r;
-cin>>n>>p;
-int v[n+1];
-if(n%p==0){
-cout<<(pow(n/p,p))%100019;
-}
-else if((n+1)%p==0){
-cout<<(pow(n/p+1,p-1)*factorial(n/p))%100019;
-}
-return 0;
+#include <cmath>
+
+#include <iomanip>
+
+#include <fstream>
+
+using namespace std;
+
+ifstream f("distantapunctdreapta.in");
+
+ofstream g("distantapunctdreapta.out");
+
+float d, X1,X2,X3,Y1,Y2,Y3,a,b,c;
+
+int main()
+
+{
+
+   f >> X1 >> Y1 >> X2 >> Y2 >> X3 >> Y3;
+
+   a=Y3-Y2; b=X2-X3; c=Y2*X3-Y3*X2;
+
+   d=abs(a*X1+b*Y1+c)/sqrt(a*a+b*b);
+
+   int m=(int)(d*100);
+
+   float z=(float)m/100;
+
+   g << fixed << setprecision(2) << z;
+
 }
