15a5bca6e0266f133971f984fb02d79168da2b7c
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-15 09:22:26 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,33 +1,24 @@
-#include <iostream>
-
-#include <cmath>
-
-#include <iomanip>
-
-#include <fstream>
+#include<iostream>
+#include<fstream>
 
 using namespace std;
 
-ifstream f("distantapunctdreapta.in");
+ifstream in("max_suma.in");
+ofstream out("max_suma.out");
 
-ofstream g("distantapunctdreapta.out");
-
-float d, X1,X2,X3,Y1,Y2,Y3,a,b,c;
-
-int main()
-
-{
-
-   f >> X1 >> Y1 >> X2 >> Y2 >> X3 >> Y3;
-
-   a=Y3-Y2; b=X2-X3; c=Y2*X3-Y3*X2;
-
-   d=abs(a*X1+b*Y1+c)/sqrt(a*a+b*b);
-
-   int m=(int)(d*100);
-
-   float z=(float)m/100;
-
-   g << fixed << setprecision(2) << z;
-
+int main() {
+  int n,m,a[50],maxim,suma;
+  in>>n>>m;
+  for (int cont=1; cont<=n;cont++ ){ 
+    for  (int i=0; i<m; i++){      
+      in>>a[i];}
+  suma = 0;
+  maxim = a[0];
+  for (int i=0; i<m; i++){   
+    suma+= a[i];   
+  if (a[i]>maxim)     
+    maxim = a[i];
+ }
+ out<<suma<<" "<<maxim<<endl;
+  }
 }
