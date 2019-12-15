46bdd26b081a14763387386b9dae1ae349b59c3c
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-15 13:16:29 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,34 +1,12 @@
-#include <bits/stdc++.h>
-#define nmax 11111130
-using namespace std;
-long long i, j, n, k, x, k1;
-int v[nmax/9];
-bool w[nmax];
-struct nod
-{
-   int xx;
-   int yy;
-} pr[100000];
-int main()
-{
-    cin >> n;
-    w[0] = w[1] = true;
-    for(i = 2; i <= n/2; i ++)
-      if(!w[i])
-        {
-            v[++ k] = i;
-            for(j = i * i; j <= n; j = i + j)
-              w[j] = true;
-        }
-    for(i = 2; i <= k; i ++)
-      {
-          x = n - v[i];
-          if(!w[x]) pr[++ k1].xx = v[i], pr[k1].yy = x;
-      }
-   for(i = 1; i <= k1; i ++)
-    cout << pr[i].xx << " " << pr[i].yy << "\n";
-   for(i = k1; i >= 1; i --)
-    cout << pr[i].yy << " " << pr[i].xx << "\n";
+#include <iostream>
+#include <algorithm> //libraria pentru functia sort()
 
-   return 0;
+using namespace std;
+
+int main()
+{
+    string s = "dacb";
+    sort(s.begin(), s.end());
+    cout << s <<endl;
+    return 0;
 }
