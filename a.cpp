63e5e785674da300ddae9debb7b5340b293bc8ab
David Pop <33874867+david7295@users.noreply.github.com>
2019-12-17 18:59:47 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,35 +1,18 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+#include <iostream>
+using namespace std;
 
-int main()
+int main ()
 {
-    int n,x,i=0,j,k,C=0,ok=0;
-    ifstream fin("ssume.in");
-    fin>>n;
-    int v[n];
-    while(fin>>x)
-    {
-        v[i]=x;
-        i++;
-    }
-    for(i=0;i<n;i++)
-    {
-        ok=0;
-        for(j=0;j<n;j++)
-            if(i!=j)
-            {
-            for(k=j+1;k<n;k++)
-              if(v[i]==v[j]+v[k] && !ok)
-               {
-                 C++;
-                 ok=1;
-               }
-            }
-    }
-    ofstream fout("ssume.out");
-    fout<<C;
-    fin.close();
-    fout.close();
-  return 0;
+
+int x,y=0,n;
+bool u=true;
+cin>>n;
+while (cin>>x&&u==true)
+{
+y++;
+if (n==y)
+u=false;
+cout <<x<<" ";
+}
+return 0;
 }
