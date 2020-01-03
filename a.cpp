a57d6631ab609e71842f76e599d0dadca17d3b0f
David Pop <33874867+david7295@users.noreply.github.com>
2020-01-04 00:34:27 +0200

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,201 +1,74 @@
-#include <iostream>
-
-#include <stack>
-
-using namespace std;
-
-struct punct{
-
-   int i,j;
-
-};
-
-int main() {
-
-   int n;
-
-   int a[100][100];
-
-   stack<punct> stiva;
-
-   cout << "n=";
-
-   cin >> n;
-
-   for (int i = 0; i < n; i++)
-
-       for (int j = 0; j < n; j++) {
-
-           cout << "a[" << i << "][" << j << "]=";
-
-           cin >> a[i][j];
-
-       }
-
-   cout << "Matricea initiala:" << endl;
-
-   for (int i = 0; i < n; i++) {
-
-       for (int j = 0; j < n; j++)
-
-           cout << a[i][j] << " ";
-
-       cout << endl;
-
-   }
-
-   int k=2;
-
-   bool sem=true;
-
-   for(int i=0; i<n && sem; i++)
-
-       for(int j=0; j<n && sem; j++)
-
-           if(a[i][j]==1)
-
-           {
-
-               struct punct p;
-
-               p.i=i;
-
-               p.j=j;
-
-               stiva.push(p);
-
-               cout<<"!!!"<<i<<" "<<j<<endl;
-
-               sem= false;
-
-           }
-
-   sem=true;
-
-   while(sem)
-
-   {
-
-       sem = false;
-
-       while(!stiva.empty())
-
-       {
-
-           struct punct p = stiva.top();
-
-           a[p.i][p.j]=k;
-
-           struct punct p2;
-
-           stiva.pop();
-
-           if(p.i>=0 && p.i<n && p.j>=0 && p.j<n)
-
-           {
-
-               if(a[p.i][p.j+1]==1 && p.j+1<n)
-
-               {
-
-                   a[p.i][p.j+1] = k;
-
-                   p2.i=p.i;
-
-                   p2.j=p.j+1;
-
-                   stiva.push(p2);
-
-               }
-
-               if(a[p.i][p.j-1]==1 && p.j-1>=0)
-
-               {
-
-                   a[p.i][p.j-1] = k;
-
-                   p2.i=p.i;
-
-                   p2.j=p.j-1;
-
-                   stiva.push(p2);
-
-               }
-
-               if(a[p.i+1][p.j]==1 && p.i+1<n)
-
-               {
-
-                   a[p.i+1][p.j] =k;
-
-                   p2.i=p.i+1;
-
-                   p2.j=p.j;
-
-                   stiva.push(p2);
-
-               }
-
-               if(a[p.i-1][p.j]==1 && p.i-1>=0)
-
-               {
-
-                   a[p.i-1][p.j] =k;
-
-                   p2.i=p.i-1;
-
-                   p2.j=p.j;
-
-                   stiva.push(p2);
-
-               }
-
-           }
-
-       }
-
-       bool sem2=true;
-
-       for(int i=0; i<n && sem2; i++)
-
-           for(int j=0; j<n && sem2; j++)
-
-               if(a[i][j]==1)
-
-               {
-
-                   struct punct p;
-
-                   p.i=i;
-
-                   p.j=j;
-
-                   stiva.push(p);
-
-                   sem2 = false;
-
-                   sem = true;
-
-                   k++;
-
-               }
-
-   }
-
-   cout << "Matricea finala:" << endl;
-
-   for (int i = 0; i < n; i++) {
-
-       for (int j = 0; j < n; j++)
-
-           cout << a[i][j] << " ";
-
-       cout << endl;
-
-   }
-
-   cout<<"Numarul de grupuri este: "<<k-1<<endl;
-
-   return 0;
-
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream fin("divizori2.in");
+ofstream fout("divizori2.out");
+
+int P,n,a[1001];
+
+int Prime(int a);
+
+int main()
+{
+
+    fin >> P >> n;
+
+    if(P==1)
+    {
+        int Max=-1;
+        for(int i=1;i<=n;i++)
+        {
+            fin >> a[i];
+            if(a[i]>Max) Max=a[i];
+        }
+
+        if(Prime(Max)) fout << 1 << " " << Max;
+        else
+        {
+            for(int i=1;i<=Max;i++)
+                if(Max%i==0) fout << i << " ";
+        }
+    }
+    else if(P==2)
+    {
+        int k=0;
+        for(int i=1;i<=n;i++)
+        {
+            fin >> a[i];
+            if(Prime(a[i])) fout << a[i] << " ", k=1;
+        }
+        if(k==0) fout << -1;
+    }
+    else
+    {
+        int Min=1000000;
+        for(int i=1;i<=n;i++)
+        {
+            fin >> a[i];
+            if(a[i]<Min) Min=a[i];
+        }
+        for(int j=1;j<=Min;j++)
+        {
+            int k=0;
+            for(int i=1;i<=n;i++)
+                if(a[i]%j==0) k++;
+            if(k==n) fout << j << " ";
+        }
+    }
+
+    return 0;
+
+}
+
+int Prime(int a)
+{
+   int i;
+   if (a == 2)
+      return 1;
+   if (a == 0 || a == 1 || a % 2 == 0)
+      return 0;
+   for(i = 3; i*i <= a && a % i; i += 2)
+      ;
+   return i*i > a;
 }
